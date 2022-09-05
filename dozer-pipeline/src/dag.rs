c164215943f7ff2f548eca7ecf7b85e04d0ba405
Matteo Pelati <>
2022-09-06 00:27:29 +0800

    working pipeline for single input

--- dozer-pipeline/src/dag.rs
+++ dozer-pipeline/src/dag.rs
@@ -19,26 +19,26 @@ impl InternalEdge {
 
 pub struct InputEdge {
     pub input_id: u8,
-    pub input: UnboundedReceiver<(u8, Record)>,
+    pub input: UnboundedReceiver<Record>,
     pub to_node: u16,
     pub to_port: u8
 }
 
 impl InputEdge {
-    pub fn new(input_id: u8, input: UnboundedReceiver<(u8, Record)>, to_node: u16, to_port: u8) -> Self {
+    pub fn new(input_id: u8, input: UnboundedReceiver<Record>, to_node: u16, to_port: u8) -> Self {
         Self { input_id, input, to_node, to_port }
     }
 }
 
 pub struct OutputEdge {
     pub output_id: u8,
-    pub output: UnboundedSender<(u8, Record)>,
+    pub output: UnboundedSender<Record>,
     pub from_node: u16,
     pub from_port: u8
 }
 
 impl OutputEdge {
-    pub fn new(output_id: u8, output: UnboundedSender<(u8, Record)>, from_node: u16, from_port: u8) -> Self {
+    pub fn new(output_id: u8, output: UnboundedSender<Record>, from_node: u16, from_port: u8) -> Self {
         Self { output_id, output, from_node, from_port }
     }
 }
@@ -53,11 +53,11 @@ pub enum Edge {
 
 pub struct Node {
     pub id: u16,
-    pub processor: Arc<Mutex<dyn Processor>>
+    pub processor: Box<dyn Processor>
 }
 
 impl Node {
-    pub fn new(id: u16, processor: Arc<Mutex<dyn Processor>>) -> Node {
+    pub fn new(id: u16, processor: Box<dyn Processor>) -> Node {
         Node{id, processor}
     }
 
@@ -82,7 +82,7 @@ impl Where {
 impl Processor for Where {
     fn process(&mut self, data: (u8, Record)) -> Vec<(u8, Record)> {
         println!("processing");
-        vec![]
+        vec![(1, Record::new(0, vec![]))]
     }
 }
 
--- dozer-pipeline/src/main.rs
+++ dozer-pipeline/src/main.rs
@@ -13,111 +13,180 @@ use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
+use futures::future::{join_all, select_all};
 use futures::task::SpawnExt;
-use futures::TryFutureExt;
 use tokio::sync::{mpsc, Mutex};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::task::{JoinHandle};
+use futures::stream::{iter};
+use tokio::{join, select};
 use crate::dag::{Edge, InputEdge, InternalEdge, Node, OutputEdge, Processor, Where};
 use crate::record::{Field, Record, Schema};
 
 
+
 async fn run_dag(nodes: Vec<Node>, edges: Vec<Edge>) {
 
-    // let mut nodes_idx: HashMap<u16, Arc<Mutex<dyn Processor>>> = HashMap::new();
-    // for node in nodes {
-    //     nodes_idx.insert(node.id, Arc::new(Mutex::new(node.processor)));
-    // }
 
-    let mut internal_senders : HashMap<u32, UnboundedSender<(u8, Record)>> = HashMap::new();
-    let mut internal_receivers : Vec<(u16, u8, UnboundedReceiver<(u8, Record)>)> = Vec::new();
+    let mut senders : HashMap<u16, HashMap<u8, UnboundedSender<Record>>> = HashMap::new();
+    let mut receivers : HashMap<u16, Vec<(u8, UnboundedReceiver<Record>)>>  = HashMap::new();
 
-    for edge in edges {
+    for node in &nodes {
+        senders.insert(node.id, HashMap::new());
+        receivers.insert(node.id, Vec::new());
+    }
 
+    for mut edge in edges {
         match edge {
             Edge::input(edge) => {
-                internal_receivers.push((edge.to_node, edge.to_port, edge.input));
+                receivers.get_mut(&edge.to_node).unwrap().push((edge.to_port, edge.input));
             }
             Edge::output(edge) => {
-                let node_port : u32 = u32::from(edge.from_node) << 16 | u32::from(edge.from_port);
-                internal_senders.insert(node_port, edge.output);
+                senders.get_mut(&edge.from_node).unwrap().insert(edge.from_port, edge.output);
             }
             Edge::internal(edge) => {
-                let (mut tx, mut rx) = mpsc::unbounded_channel::<(u8,Record)>();
-                let from_node_port : u32 = u32::from(edge.from_node) << 16 | u32::from(edge.from_port);
-                internal_senders.insert(from_node_port, tx);
-                let to_node_port : u32 = u32::from(edge.to_node) << 16 | u32::from(edge.to_port);
-                internal_receivers.push((edge.to_node, edge.to_port, rx));
+                let (mut tx, mut rx) = mpsc::unbounded_channel::<Record>();
+                receivers.get_mut(&edge.to_node).unwrap().push((edge.to_port, rx)) ;
+                senders.get_mut(&edge.from_node).unwrap().insert(edge.from_port, tx);
             }
         }
     }
 
     let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
-    for t in internal_receivers {
+    for mut node in nodes {
 
-        let mut receiver = t.2;
-        let mut cloned_processor = nodes.first().unwrap().processor.clone();
+        let mut node_receivers = receivers.remove(&node.id).unwrap();
+        let mut node_senders = senders.remove(&node.id).unwrap();
 
-        handles.push(tokio::spawn(async move {
+        if node_receivers.len() == 1 {
 
-            let result = receiver.recv().and_then(async {
+            let mut receiver = node_receivers.remove(0);
+            let handle = tokio::spawn(async move {
 
-            })
-
-            loop {
-                println!("starting to listen");
-                let result = receiver.recv().await;
-                if result.is_none() {
-                    return;
+                let mut senders = node_senders;
+                loop {
+                    let res = receiver.1.recv().await;
+                    if res.is_none() {
+                        println!("Exiting read loop for node/port {}/{}", node.id, receiver.0);
+                        return;
+                    }
+                    println!("Incoming record on node {} / port {}", node.id, receiver.0);
+                    let processed = node.processor.process((receiver.0, res.unwrap()));
+                    for rec in processed {
+                        let sender = senders.get_mut(&rec.0);
+                        if (!sender.is_none()) {
+                            println!("Forwarding message from node {} / port {}", node.id, rec.0);
+                            sender.unwrap().send(rec.1);
+                        }
+                    }
                 }
-                cloned_processor.lock().await.process(result.unwrap());
-                //cloned_processor.get_mut().process(result.unwrap());
-                println!("something received");
+            });
+        }
+        else {
+
+            let mut m_node_senders : HashMap<u8, Arc<Mutex<UnboundedSender<Record>>>> = HashMap::new();
+            for mut t in node_senders {
+                m_node_senders.insert(t.0, Arc::new(Mutex::new(t.1)));
             }
-        }));
+
+            let node_id = node.id;
+            let mut m_node_processor = Arc::new(Mutex::new(node.processor));
+
+            for mut receiver in node_receivers {
+
+                let mut m_node_processor_clone = m_node_processor.clone();
+                let mut m_node_senders_clone : HashMap<u8, Arc<Mutex<UnboundedSender<Record>>>> = HashMap::new();
+                for mut t in &m_node_senders.clone() {
+                    m_node_senders.insert(t.0.clone(), t.1.clone());
+                }
+
+                let handle = tokio::spawn(async move {
+
+                    let res = receiver.1.recv().await;
+                    if res.is_none() {
+                        println!("Exiting read loop for node/port {}/{}", node_id, receiver.0);
+                        return;
+                    }
+                    println!("Incoming record on node {} / port {}", node_id, receiver.0);
+                    let processed = m_node_processor_clone.lock().await.process((receiver.0, res.unwrap()));
+                    for rec in processed {
+                        let sender = m_node_senders_clone.get_mut(&rec.0);
+                        if (!sender.is_none()) {
+                            println!("Forwarding message from node {} / port {}", node.id, rec.0);
+                            sender.unwrap().lock().await.send(rec.1);
+                        }
+                    }
+                });
+            }
+
+
+        }
+
     }
 
     futures::future::join_all(handles).await;
 
 }
 
-async fn sender(tx: UnboundedSender<(u8, Record)>) {
+async fn sender(tx: UnboundedSender<Record>) {
 
-    while true {
-        sleep(Duration::from_secs(1));
-        let r = tx.send((1, Record::new(1, vec![])));
+    let mut ctr = 0;
+    println!("Starting sender");
+    loop {
+        ctr += 1;
+        println!("record {}", &ctr);
+        let r = tx.send(Record::new(1, vec![]));
         if r.is_err() {
             println!("Error sending");
         }
     }
 
+}
+
+async fn receiver(mut rx: UnboundedReceiver<Record>) {
+
+    println!("Starting receiver");
+    loop {
+        let r = rx.recv().await;
+        if (r.is_none()) {
+            return;
+        }
+        println!("Received");
+    }
 
 }
 
 #[tokio::main]
 async fn main() {
 
-    let (mut input_tx, mut input_rx) = mpsc::unbounded_channel::<(u8,Record)>();
-    let (mut output_tx, mut output_rx) = mpsc::unbounded_channel::<(u8,Record)>();
+    let (mut input_tx, mut input_rx) = mpsc::unbounded_channel::<Record>();
+    let (mut input2_tx, mut input2_rx) = mpsc::unbounded_channel::<Record>();
+    let (mut output_tx, mut output_rx) = mpsc::unbounded_channel::<Record>();
 
     let nodes = vec![
-        Node::new(100, Arc::new(Mutex::new(Where::new()))),
-        Node::new(200, Arc::new(Mutex::new(Where::new()))),
-        Node::new(300, Arc::new(Mutex::new(Where::new())))
+        Node::new(100, Box::new(Where::new())),
+        Node::new(200, Box::new(Where::new())),
+        Node::new(300, Box::new(Where::new())),
+        Node::new(400, Box::new(Where::new()))
     ];
 
     let edges = vec![
         Edge::input(InputEdge::new(1, input_rx, 100, 1)),
+    //    Edge::input(InputEdge::new(2, input2_rx, 100, 2)),
         Edge::internal(InternalEdge::new(100, 1, 200, 1)),
         Edge::internal(InternalEdge::new(200, 1, 300, 1)),
-        Edge::output(OutputEdge::new(1, output_tx, 300,1))
+        Edge::internal(InternalEdge::new(300, 1, 400, 1)),
+        Edge::output(OutputEdge::new(1, output_tx, 400,1))
     ];
 
 
-    let r1 = run_dag(nodes, edges);
-    let r2 = sender(input_tx);
-    futures::future::join(r1, r2).await;
+    let r1 = tokio::spawn(run_dag(nodes, edges));
+    let r3 = tokio::spawn(receiver(output_rx));
+    let r2 = tokio::spawn(sender(input_tx));
+   // let r4 = tokio::spawn(sender(input2_tx));
+
+    futures::future::join3(r1, r2, r3).await;
 
 
 
