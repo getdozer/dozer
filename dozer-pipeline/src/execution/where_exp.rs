4ce29d096f286d8a835accab6a152a8b7b1a2fd7
Matteo Pelati <>
2022-09-07 14:44:51 +0800

    added namespace for tests

--- dozer-pipeline/src/execution/where_exp.rs
+++ dozer-pipeline/src/execution/where_exp.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_must_use)]
 
 use std::ops::Deref;
 use crate::{ExecutionContext, Field, MemoryExecutionContext, Operation, Processor, Record};
@@ -140,143 +141,131 @@ pub fn eval_operator(op: &Box<Operator>, record: &Record, ctx: &dyn ExecutionCon
     }
 }
 
+mod tests {
+    use crate::{Field, MemoryExecutionContext, Record};
+    use crate::execution::where_exp::{eval_operator, Operand, Operator};
+
+    #[test]
+    fn test_eq_operator_int() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::int_field(10);
+        let r = Record::new(0, vec![Field::int_field(10)]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
+
+    #[test]
+    fn test_eq_operator_float() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::float_field(10.1);
+        let r = Record::new(0, vec![Field::float_field(10.1)]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
+
+    #[test]
+    fn test_eq_operator_bool() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::bool_field(false);
+        let r = Record::new(0, vec![Field::bool_field(false)]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
+
+    #[test]
+    fn test_eq_operator_str() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::string_field("test".to_string());
+        let r = Record::new(0, vec![Field::string_field("test".to_string())]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
+
+    #[test]
+    fn test_eq_operator_binary() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::binary_field(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        let r = Record::new(0, vec![Field::binary_field(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
+
+    #[test]
+    fn test_eq_operator_ts() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::timestamp_field(1000);
+        let r = Record::new(0, vec![Field::timestamp_field(1000)]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
 
 
-#[test]
-fn test_eq_operator_int() {
+    #[test]
+    fn test_ne_operator() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::int_field(50);
+        let r = Record::new(0, vec![Field::int_field(10)]);
+        let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::int_field(10);
-    let r = Record::new(0, vec![Field::int_field(10)]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+    #[test]
+    fn test_gt_operator() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::int_field(50);
+        let r = Record::new(0, vec![Field::int_field(100)]);
+        let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
 
-#[test]
-fn test_eq_operator_float() {
+    #[test]
+    fn test_lt_operator() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::int_field(100);
+        let r = Record::new(0, vec![Field::int_field(50)]);
+        let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
+        assert!(eval_operator(&op, &r, &ctx));
+    }
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::float_field(10.1);
-    let r = Record::new(0, vec![Field::float_field(10.1)]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+    #[test]
+    fn test_and_operator() {
+        let ctx = MemoryExecutionContext::new();
 
-#[test]
-fn test_eq_operator_bool() {
+        let f0 = Field::int_field(10);
+        let f1 = Field::int_field(20);
+        let r = Record::new(0, vec![Field::int_field(10), Field::int_field(20)]);
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::bool_field(false);
-    let r = Record::new(0, vec![Field::bool_field(false)]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+        let op0 = Box::new(Operator::eq(Operand::const_value(f0), Operand::field_value(0)));
+        let op1 = Box::new(Operator::eq(Operand::const_value(f1), Operand::field_value(1)));
 
-#[test]
-fn test_eq_operator_str() {
+        let and = Box::new(Operator::and(op0, op1));
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::string_field("test".to_string());
-    let r = Record::new(0, vec![Field::string_field("test".to_string())]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+        assert!(eval_operator(&and, &r, &ctx));
+    }
 
-#[test]
-fn test_eq_operator_binary() {
+    #[test]
+    fn test_or_operator() {
+        let ctx = MemoryExecutionContext::new();
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::binary_field(vec![1,2,3,4,5,6,7,8,9,10]);
-    let r = Record::new(0, vec![Field::binary_field(vec![1,2,3,4,5,6,7,8,9,10])]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+        let f0 = Field::int_field(10);
+        let f1 = Field::int_field(30);
+        let r = Record::new(0, vec![Field::int_field(10), Field::int_field(20)]);
 
-#[test]
-fn test_eq_operator_ts() {
+        let op0 = Box::new(Operator::eq(Operand::const_value(f0), Operand::field_value(0)));
+        let op1 = Box::new(Operator::eq(Operand::const_value(f1), Operand::field_value(1)));
 
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::timestamp_field(1000);
-    let r = Record::new(0, vec![Field::timestamp_field(1000)]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
+        let and = Box::new(Operator::or(op0, op1));
 
+        assert!(eval_operator(&and, &r, &ctx));
+    }
 
-#[test]
-fn test_ne_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::int_field(50);
-    let r = Record::new(0, vec![Field::int_field(10)]);
-    let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
-
-#[test]
-fn test_gt_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::int_field(50);
-    let r = Record::new(0, vec![Field::int_field(100)]);
-    let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
-
-#[test]
-fn test_lt_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::int_field(100);
-    let r = Record::new(0, vec![Field::int_field(50)]);
-    let op = Box::new(Operator::ne(Operand::const_value(f), Operand::field_value(0)));
-    assert!(eval_operator(&op, &r, &ctx));
-}
-
-#[test]
-fn test_and_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-
-    let f0 = Field::int_field(10);
-    let f1 = Field::int_field(20);
-    let r = Record::new(0, vec![Field::int_field(10), Field::int_field(20)]);
-
-    let op0 = Box::new(Operator::eq(Operand::const_value(f0), Operand::field_value(0)));
-    let op1 = Box::new(Operator::eq(Operand::const_value(f1), Operand::field_value(1)));
-
-    let and = Box::new(Operator::and(op0, op1));
-
-    assert!(eval_operator(&and, &r, &ctx));
-
-}
-
-#[test]
-fn test_or_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-
-    let f0 = Field::int_field(10);
-    let f1 = Field::int_field(30);
-    let r = Record::new(0, vec![Field::int_field(10), Field::int_field(20)]);
-
-    let op0 = Box::new(Operator::eq(Operand::const_value(f0), Operand::field_value(0)));
-    let op1 = Box::new(Operator::eq(Operand::const_value(f1), Operand::field_value(1)));
-
-    let and = Box::new(Operator::or(op0, op1));
-
-    assert!(eval_operator(&and, &r, &ctx));
-
-}
-
-#[test]
-fn test_not_operator() {
-
-    let ctx = MemoryExecutionContext::new();
-    let f = Field::int_field(10);
-    let r = Record::new(0, vec![Field::int_field(20)]);
-    let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
-    let not_op = Box::new(Operator::not(op));
-    assert!(eval_operator(&not_op, &r, &ctx));
-
+    #[test]
+    fn test_not_operator() {
+        let ctx = MemoryExecutionContext::new();
+        let f = Field::int_field(10);
+        let r = Record::new(0, vec![Field::int_field(20)]);
+        let op = Box::new(Operator::eq(Operand::const_value(f), Operand::field_value(0)));
+        let not_op = Box::new(Operator::not(op));
+        assert!(eval_operator(&not_op, &r, &ctx));
+    }
 }
