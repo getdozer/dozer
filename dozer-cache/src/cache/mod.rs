ed7ce47261063a78f53790fa9481e6cbdfc5568b
VG <vivekg342@gmail.com>
2022-09-25 13:10:28 +0800

    Initialize dozer pipeline

---
+++ dozer-cache/src/cache/mod.rs
@@ -0,0 +1,3 @@
+pub mod cache;
+pub mod indexer;
+pub mod utils;
---
+++ dozer-cache/src/cache/utils.rs
@@ -0,0 +1,26 @@
+use lmdb::{Database, DatabaseFlags, Environment, Transaction, WriteFlags};
+use std::fs;
+use std::path::Path;
+
+pub fn init_db<'a>() -> (Environment, Database) {
+    match fs::create_dir_all("target/cache.mdb") {
+        Ok(_) => {}
+        Err(_) => println!("path already exists"),
+    };
+
+    let env = Environment::new()
+        .open(
+            Path::new("target/cache.mdb")
+                .canonicalize()
+                .unwrap()
+                .as_path(),
+        )
+        .unwrap();
+
+    let mut flags = DatabaseFlags::default();
+    flags.set(DatabaseFlags::DUP_SORT, true);
+
+    let db = env.create_db(None, flags).unwrap();
+
+    (env, db)
+}
