55a8869fced89604baf4c71cc496d603a685333c
VG <vivekg342@gmail.com>
2022-09-30 17:01:06 +0800

    feat: implement secondary indexes (#49)
    * feat: implement secondary indexes
    
    * chore: refactor index
    
    * chore: fix cursor
    
    * chore: uncomment tests

--- dozer-cache/src/cache/lmdb/cache.rs
+++ dozer-cache/src/cache/lmdb/cache.rs
@@ -1,15 +1,19 @@
 use std::sync::Arc;
 
+use crate::cache::expression::Expression;
+use crate::cache::get_primary_key;
+
 use super::super::Cache;
+use super::indexer::Indexer;
+use super::query::QueryHandler;
 use super::utils;
 use async_trait::async_trait;
 use dozer_schema::registry::context::Context;
 use dozer_schema::registry::SchemaRegistryClient;
 use dozer_schema::storage::get_schema_key;
-use dozer_types::types::{Field, Schema, SchemaIdentifier};
 use dozer_types::types::{Operation, Record};
-use lmdb::{Database, Environment, RoTransaction, RwTransaction, Transaction, WriteFlags};
-
+use dozer_types::types::{Schema, SchemaIdentifier};
+use lmdb::{Cursor, Database, Environment, RoTransaction, RwTransaction, Transaction, WriteFlags};
 pub struct LmdbCache {
     env: Environment,
     db: Database,
@@ -39,32 +43,37 @@ impl LmdbCache {
         let mut txn: RwTransaction = self.env.begin_rw_txn().unwrap();
         let p_key = schema.primary_index.clone();
         let values = rec.values.clone();
-        let key = self.get_key(p_key, values.to_owned());
+        let key = get_primary_key(p_key, values.to_owned());
 
         let encoded: Vec<u8> = bincode::serialize(&rec).unwrap();
 
         txn.put::<Vec<u8>, Vec<u8>>(self.db, &key, &encoded, WriteFlags::default())?;
 
-        txn.commit().unwrap();
+        let indexer = Indexer::new(&self.db);
 
+        indexer.build_indexes(&mut txn, rec, schema, key)?;
+
+        txn.commit()?;
+
+        Ok(())
+    }
+
+    fn _debug_dump(&self) -> anyhow::Result<()> {
+        let txn: RoTransaction = self.env.begin_ro_txn().unwrap();
+        let cursor = txn.open_ro_cursor(self.db)?;
+        loop {
+            if let Ok((key, val)) = cursor.get(None, None, 8) {
+                println!("key: {:?}, val: {:?}", key.unwrap(), val);
+            } else {
+                break;
+            };
+        }
         Ok(())
     }
 }
 
 #[async_trait]
 impl Cache for LmdbCache {
-    fn get_key(&self, primary_index: Vec<usize>, values: Vec<Field>) -> Vec<u8> {
-        let key: Vec<Vec<u8>> = primary_index
-            .iter()
-            .map(|idx| {
-                let field = values[*idx].clone();
-                let encoded: Vec<u8> = bincode::serialize(&field).unwrap();
-                encoded
-            })
-            .collect();
-
-        key.join("#".as_bytes())
-    }
     async fn insert(&self, rec: Record) -> anyhow::Result<()> {
         let schema_identifier = rec.schema_id.clone().unwrap();
         let schema = match self.get_schema(schema_identifier.clone()).await {
@@ -98,8 +107,22 @@ impl Cache for LmdbCache {
         Ok(rec)
     }
 
-    async fn query(&self, _key: Vec<u8>) -> anyhow::Result<Vec<Record>> {
-        todo!()
+    async fn query(
+        &self,
+        schema_identifier: SchemaIdentifier,
+        exp: Expression,
+    ) -> anyhow::Result<Vec<Record>> {
+        let schema = self.get_schema(schema_identifier.clone()).await?;
+
+        let handler = QueryHandler::new(&self.env, &self.db);
+        let pkeys = handler.query(schema, exp)?;
+        let mut records = vec![];
+        for key in pkeys.iter() {
+            let key = key.clone();
+            let rec = self.get(key).await?;
+            records.push(rec);
+        }
+        Ok(records)
     }
 
     async fn handle_batch(&self, _operations: Vec<Operation>) -> anyhow::Result<()> {
@@ -129,21 +152,28 @@ mod tests {
     use std::sync::Arc;
 
     use super::LmdbCache;
-    use crate::cache::Cache;
+    use crate::cache::{
+        expression::{self, Expression},
+        get_primary_key, Cache,
+    };
     use dozer_schema::{
         registry::{SchemaRegistryClient, _serve_channel, client},
         test_helper::init_schema,
     };
-    use dozer_types::types::{Field, Record};
+    use dozer_types::types::{Field, Record, Schema};
 
-    #[tokio::test]
-    async fn insert_and_get_schema() -> anyhow::Result<()> {
+    async fn _setup() -> (LmdbCache, Schema) {
         let client_transport = _serve_channel().unwrap();
         let client = Arc::new(
             SchemaRegistryClient::new(client::Config::default(), client_transport).spawn(),
         );
         let schema = init_schema(client.clone()).await;
         let cache = LmdbCache::new(client.clone(), true);
+        (cache, schema)
+    }
+    #[tokio::test]
+    async fn insert_and_get_schema() -> anyhow::Result<()> {
+        let (cache, schema) = _setup().await;
         cache.insert_schema(schema.clone()).await?;
 
         let get_schema = cache.get_schema(schema.identifier.clone().unwrap()).await?;
@@ -153,20 +183,11 @@ mod tests {
     #[tokio::test]
     async fn insert_get_and_delete_record() -> anyhow::Result<()> {
         let val = "bar".to_string();
-
-        let client_transport = _serve_channel().unwrap();
-        let client = Arc::new(
-            SchemaRegistryClient::new(client::Config::default(), client_transport).spawn(),
-        );
-        let schema = init_schema(client.clone()).await;
-
+        let (cache, schema) = _setup().await;
         let record = Record::new(schema.identifier.clone(), vec![Field::String(val.clone())]);
-
-        let cache = LmdbCache::new(client.clone(), true);
-
         cache.insert(record.clone()).await?;
 
-        let key = cache.get_key(vec![0], vec![Field::String(val)]);
+        let key = get_primary_key(vec![0], vec![Field::String(val)]);
 
         let get_record = cache.get(key.clone()).await?;
         assert_eq!(get_record, record.clone(), "must be equal");
@@ -174,6 +195,27 @@ mod tests {
         cache.delete(key.clone()).await?;
 
         cache.get(key).await.expect_err("Must not find a record");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn insert_and_query_record() -> anyhow::Result<()> {
+        let val = "bar".to_string();
+        let (cache, schema) = _setup().await;
+        let record = Record::new(schema.identifier.clone(), vec![Field::String(val.clone())]);
+
+        cache.insert(record.clone()).await?;
+
+        let exp = Expression::Simple(
+            "foo".to_string(),
+            expression::Comparator::EQ,
+            Field::String("bar".to_string()),
+        );
+        let records = cache.query(schema.identifier.unwrap(), exp).await?;
+
+        println!("records: {:?}", records);
+        assert_eq!(records[0], record.clone(), "must be equal");
 
         Ok(())
     }
