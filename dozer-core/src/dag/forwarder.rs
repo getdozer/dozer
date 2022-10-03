a5be2acb39f9e96ceb4cc6b892791b6a1c9f75b6
Matteo Pelati <>
2022-10-03 18:43:14 +0800

    ref: Refactored channel manager

---
+++ dozer-core/src/dag/forwarder.rs
@@ -0,0 +1,103 @@
+use std::collections::HashMap;
+use std::thread::sleep;
+use std::time::Duration;
+use anyhow::anyhow;
+use crossbeam::channel::Sender;
+use dozer_types::types::{Operation, OperationEvent};
+use crate::dag::dag::PortHandle;
+
+pub trait SourceChannelForwarder {
+    fn send(&self, op: OperationEvent, port: PortHandle) -> anyhow::Result<()>;
+}
+
+pub trait ProcessorChannelForwarder {
+    fn send(&self, op: Operation, port: PortHandle) -> anyhow::Result<()>;
+}
+
+pub trait ChannelManager {
+    fn terminate(&self) -> anyhow::Result<()>;
+}
+
+pub struct LocalChannelForwarder {
+    senders: HashMap<PortHandle, Vec<Sender<OperationEvent>>>,
+    curr_seq_no: u64
+}
+
+impl LocalChannelForwarder {
+
+    pub fn new(senders: HashMap<PortHandle, Vec<Sender<OperationEvent>>>) -> Self {
+        Self { senders, curr_seq_no: 0 }
+    }
+
+    pub fn update_seq_no(&mut self, seq: u64) {
+        self.curr_seq_no = seq;
+    }
+
+    fn send_op(&self, op: Operation, port_id: PortHandle) -> anyhow::Result<()> {
+        let e = OperationEvent::new(self.curr_seq_no, op);
+        self.send_opevent(e, port_id)
+    }
+
+    fn send_opevent(&self, op: OperationEvent, port_id: PortHandle) -> anyhow::Result<()> {
+
+        let senders = self.senders.get(&port_id);
+        if senders.is_none() {
+            return Err(anyhow!("Invalid output port".to_string()));
+        }
+
+        if senders.unwrap().len() == 1 {
+            senders.unwrap()[0].send(op)?;
+        }
+        else {
+            for sender in senders.unwrap() {
+                sender.send(op.clone())?;
+            }
+        }
+
+        return Ok(());
+    }
+
+    pub fn terminate(&self) -> anyhow::Result<()> {
+        for senders in &self.senders {
+
+            for sender in senders.1 {
+                sender.send(OperationEvent::new(0, Operation::Terminate))?;
+            }
+
+            loop {
+                let mut is_empty = true;
+                for senders in &self.senders {
+                    for sender in senders.1 {
+                        is_empty |= sender.is_empty();
+                    }
+                }
+
+                if !is_empty {
+                    sleep(Duration::from_millis(250));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return Ok(());
+    }
+}
+
+impl SourceChannelForwarder for LocalChannelForwarder {
+    fn send(&self, op: OperationEvent, port: PortHandle) -> anyhow::Result<()> {
+        self.send_opevent(op, port)
+    }
+}
+
+impl ProcessorChannelForwarder for LocalChannelForwarder {
+    fn send(&self, op: Operation, port: PortHandle) -> anyhow::Result<()> {
+        self.send_op(op, port)
+    }
+}
+
+impl ChannelManager for LocalChannelForwarder {
+    fn terminate(&self) -> anyhow::Result<()> {
+        self.terminate()
+    }
+}
