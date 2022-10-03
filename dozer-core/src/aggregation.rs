d73633d554b55864bf14eb34a66220920f43c1c5
Matteo Pelati <>
2022-10-04 00:23:44 +0800

    ref: groupby refactor + processor impl

--- dozer-core/src/aggregation.rs
+++ dozer-core/src/aggregation.rs
@@ -1,14 +1,19 @@
-use dozer_types::types::{Field, Record};
+use dozer_types::types::{Field, FieldType, Record};
+use dyn_clone::DynClone;
 
 mod groupby;
 mod sum;
 mod tests;
 
 
-pub trait Aggregator {
+
+pub trait Aggregator : DynClone + Send + Sync {
+    fn get_return_type(&self) -> FieldType;
     fn get_type(&self) -> u8;
-    fn insert(&self, curr_state: Option<&[u8]>, new: &Record) -> anyhow::Result<Vec<u8>>;
-    fn update(&self, curr_state: Option<&[u8]>, old: &Record, new: &Record) -> anyhow::Result<Vec<u8>>;
-    fn delete(&self, curr_state: Option<&[u8]>, old: &Record) -> anyhow::Result<Vec<u8>>;
+    fn insert(&self, curr_state: Option<&[u8]>, new: &Field) -> anyhow::Result<Vec<u8>>;
+    fn update(&self, curr_state: Option<&[u8]>, old: &Field, new: &Field) -> anyhow::Result<Vec<u8>>;
+    fn delete(&self, curr_state: Option<&[u8]>, old: &Field) -> anyhow::Result<Vec<u8>>;
     fn get_value(&self, f: &[u8]) -> Field;
 }
+
+dyn_clone::clone_trait_object!(Aggregator);
