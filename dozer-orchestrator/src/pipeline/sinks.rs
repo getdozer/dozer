0eabc05bb996956143b9bba166c2f64bf23da4a1
VG <vivekg342@gmail.com>
2022-10-15 16:57:01 +0800

    chore: fix clippy warning

--- dozer-orchestrator/src/pipeline/sinks.rs
+++ dozer-orchestrator/src/pipeline/sinks.rs
@@ -89,7 +89,7 @@ impl Sink for CacheSink {
         // Get hash of schema
         let mut hasher = DefaultHasher::new();
         let bytes = self.api_endpoint.sql.as_bytes();
-        hasher.write(&bytes);
+        hasher.write(bytes);
         let hash = hasher.finish();
 
         schema.identifier = Some(SchemaIdentifier {
