c088d5d958846229854ca241544991d84a8d07bb
chubei <914745487@qq.com>
2022-10-16 15:42:17 +0800

    `first` flag in `CacheIteratorState` was not set correctly

--- dozer-cache/src/cache/lmdb/query/iterator.rs
+++ dozer-cache/src/cache/lmdb/query/iterator.rs
@@ -1,28 +1,53 @@
 use lmdb::{Cursor, RoCursor};
+use lmdb_sys::{MDB_FIRST, MDB_LAST, MDB_NEXT, MDB_PREV, MDB_SET_RANGE};
 use log::debug;
+
+enum CacheIteratorState<'a> {
+    First {
+        starting_key: Option<&'a Vec<u8>>,
+        ascending: bool,
+    },
+    NotFirst {
+        ascending: bool,
+    },
+}
 
 pub struct CacheIterator<'a> {
     cursor: &'a RoCursor<'a>,
-    starting_key: Option<&'a Vec<u8>>,
-    ascending: bool,
-    first: bool,
+    state: CacheIteratorState<'a>,
 }
 
 impl<'a> Iterator for CacheIterator<'a> {
     type Item = (&'a [u8], &'a [u8]);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let key: Option<&[u8]> = if self.starting_key.is_none() {
-            None
-        } else {
-            Some(self.starting_key.as_ref().unwrap())
+        let res = match self.state {
+            CacheIteratorState::First {
+                starting_key,
+                ascending,
+            } => {
+                let res = match starting_key {
+                    Some(starting_key) => self.cursor.get(Some(starting_key), None, MDB_SET_RANGE),
+                    None => {
+                        if ascending {
+                            self.cursor.get(None, None, MDB_FIRST)
+                        } else {
+                            self.cursor.get(None, None, MDB_LAST)
+                        }
+                    }
+                };
+                self.state = CacheIteratorState::NotFirst { ascending };
+                res
+            }
+            CacheIteratorState::NotFirst { ascending } => {
+                if ascending {
+                    self.cursor.get(None, None, MDB_NEXT)
+                } else {
+                    self.cursor.get(None, None, MDB_PREV)
+                }
+            }
         };
 
-        if self.first {
-            self.first = false;
-        }
-        let next_op = Self::get_next_op(self.starting_key.is_some(), self.ascending, self.first);
-        let res = self.cursor.get(key, None, next_op.get_value());
         match res {
             Ok((key, val)) => key.map(|key| (key, val)),
             Err(e) => {
@@ -36,52 +61,10 @@ impl<'a> CacheIterator<'a> {
     pub fn new(cursor: &'a RoCursor, starting_key: Option<&'a Vec<u8>>, ascending: bool) -> Self {
         CacheIterator {
             cursor,
-            starting_key,
-            ascending,
-            first: true,
-        }
-    }
-
-    fn get_next_op(has_expression: bool, ascending: bool, first: bool) -> NextOp {
-        let next_op = if first && has_expression {
-            NextOp::First
-        } else {
-            NextOp::Next
-        };
-
-        if !ascending {
-            match next_op {
-                NextOp::First => NextOp::Last,
-                NextOp::Last => NextOp::First,
-                NextOp::Next => NextOp::Prev,
-                NextOp::Prev => NextOp::Next,
-            }
-        } else {
-            next_op
-        }
-    }
-}
-
-// http://www.lmdb.tech/doc/group__mdb.html#ga1206b2af8b95e7f6b0ef6b28708c9127
-pub const MDB_FIRST: u32 = 0;
-pub const MDB_LAST: u32 = 6;
-pub const MDB_NEXT: u32 = 8;
-pub const MDB_PREV: u32 = 12;
-pub const MDB_SET_RANGE: u32 = 17;
-
-enum NextOp {
-    First,
-    Last,
-    Next,
-    Prev,
-}
-impl NextOp {
-    pub fn get_value(&self) -> u32 {
-        match self {
-            NextOp::First => MDB_SET_RANGE,
-            NextOp::Last => MDB_LAST,
-            NextOp::Next => MDB_NEXT,
-            NextOp::Prev => MDB_PREV,
+            state: CacheIteratorState::First {
+                starting_key: starting_key.map(AsRef::as_ref),
+                ascending,
+            },
         }
     }
 }
