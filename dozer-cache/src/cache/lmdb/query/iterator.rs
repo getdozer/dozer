db3ac2fa9ccb59de96b517c51e31602677f18bd9
chubei <914745487@qq.com>
2022-10-16 19:19:20 +0800

    fix: `first` flag in `CacheIterator` was not set correctly (#101)
    * Add unit test to expose bug in `CacheIterator`
    
    * `first` flag in `CacheIteratorState` was not set correctly
    
    * Add more unit tests and fix descending from non-existing key

--- dozer-cache/src/cache/lmdb/query/iterator.rs
+++ dozer-cache/src/cache/lmdb/query/iterator.rs
@@ -1,28 +1,68 @@
 use lmdb::{Cursor, RoCursor};
+use lmdb_sys::{MDB_FIRST, MDB_LAST, MDB_NEXT, MDB_PREV, MDB_SET_RANGE};
 use log::debug;
+
+enum CacheIteratorState<'a> {
+    First {
+        starting_key: Option<&'a Vec<u8>>,
+        ascending: bool,
+    },
+    NotFirst {
+        ascending: bool,
+    },
+}
 
 pub struct CacheIterator<'a> {
     cursor: &'a RoCursor<'a>,
-    starting_key: Option<&'a Vec<u8>>,
-    ascending: bool,
-    first: bool,
+    state: CacheIteratorState<'a>,
 }
 
 impl<'a> Iterator for CacheIterator<'a> {
     type Item = (&'a [u8], &'a [u8]);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let key: Option<&[u8]> = if self.starting_key.is_none() {
-            None
-        } else {
-            Some(self.starting_key.as_ref().unwrap())
+        let res = match self.state {
+            CacheIteratorState::First {
+                starting_key,
+                ascending,
+            } => {
+                let res = match starting_key {
+                    Some(starting_key) => {
+                        if ascending {
+                            self.cursor.get(Some(starting_key), None, MDB_SET_RANGE)
+                        } else {
+                            let (key, val) =
+                                match self.cursor.get(Some(starting_key), None, MDB_SET_RANGE) {
+                                    Ok((Some(key), val)) => (key, val),
+                                    _ => return None,
+                                };
+                            if key == starting_key {
+                                Ok((Some(key), val))
+                            } else {
+                                self.cursor.get(None, None, MDB_PREV)
+                            }
+                        }
+                    }
+                    None => {
+                        if ascending {
+                            self.cursor.get(None, None, MDB_FIRST)
+                        } else {
+                            self.cursor.get(None, None, MDB_LAST)
+                        }
+                    }
+                };
+                self.state = CacheIteratorState::NotFirst { ascending };
+                res
+            }
+            CacheIteratorState::NotFirst { ascending } => {
+                if ascending {
+                    self.cursor.get(None, None, MDB_NEXT)
+                } else {
+                    self.cursor.get(None, None, MDB_PREV)
+                }
+            }
         };
 
-        if self.first {
-            self.first = false;
-        }
-        let next_op = Self::get_next_op(self.starting_key.is_some(), self.ascending, self.first);
-        let res = self.cursor.get(key, None, next_op.get_value());
         match res {
             Ok((key, val)) => key.map(|key| (key, val)),
             Err(e) => {
@@ -36,52 +76,112 @@ impl<'a> CacheIterator<'a> {
     pub fn new(cursor: &'a RoCursor, starting_key: Option<&'a Vec<u8>>, ascending: bool) -> Self {
         CacheIterator {
             cursor,
-            starting_key,
-            ascending,
-            first: true,
+            state: CacheIteratorState::First {
+                starting_key: starting_key.map(AsRef::as_ref),
+                ascending,
+            },
         }
     }
+}
 
-    fn get_next_op(has_expression: bool, ascending: bool, first: bool) -> NextOp {
-        let next_op = if first && has_expression {
-            NextOp::First
-        } else {
-            NextOp::Next
+#[cfg(test)]
+mod tests {
+    use lmdb::{Transaction, WriteFlags};
+
+    use crate::cache::lmdb::utils::{init_db, init_env};
+
+    use super::CacheIterator;
+
+    #[test]
+    fn test_cache_iterator() {
+        let env = init_env(true).unwrap();
+        let db = init_db(&env, None).unwrap();
+
+        // Insert test data.
+        let mut txn = env.begin_rw_txn().unwrap();
+        for key in [
+            b"aa", b"ab", b"ac", b"ba", b"bb", b"bc", b"ca", b"cb", b"cc",
+        ] {
+            txn.put(db, key, &[], WriteFlags::empty()).unwrap();
+        }
+        txn.commit().unwrap();
+
+        // Create testing cursor and utility function.
+        let txn = env.begin_ro_txn().unwrap();
+        let cursor = txn.open_ro_cursor(db).unwrap();
+        let check = |starting_key, ascending, expected: Vec<&'static [u8]>| {
+            let actual = CacheIterator::new(&cursor, starting_key, ascending)
+                .map(|(key, _)| key)
+                .collect::<Vec<_>>();
+            assert_eq!(actual, expected);
         };
 
-        if !ascending {
-            match next_op {
-                NextOp::First => NextOp::Last,
-                NextOp::Last => NextOp::First,
-                NextOp::Next => NextOp::Prev,
-                NextOp::Prev => NextOp::Next,
-            }
-        } else {
-            next_op
-        }
-    }
-}
+        // Test ascending from start.
+        check(
+            None,
+            true,
+            vec![
+                b"aa", b"ab", b"ac", b"ba", b"bb", b"bc", b"ca", b"cb", b"cc",
+            ],
+        );
 
-// http://www.lmdb.tech/doc/group__mdb.html#ga1206b2af8b95e7f6b0ef6b28708c9127
-pub const MDB_FIRST: u32 = 0;
-pub const MDB_LAST: u32 = 6;
-pub const MDB_NEXT: u32 = 8;
-pub const MDB_PREV: u32 = 12;
-pub const MDB_SET_RANGE: u32 = 17;
+        // Test ascending from start using the same cursor again.
+        check(
+            None,
+            true,
+            vec![
+                b"aa", b"ab", b"ac", b"ba", b"bb", b"bc", b"ca", b"cb", b"cc",
+            ],
+        );
 
-enum NextOp {
-    First,
-    Last,
-    Next,
-    Prev,
-}
-impl NextOp {
-    pub fn get_value(&self) -> u32 {
-        match self {
-            NextOp::First => MDB_SET_RANGE,
-            NextOp::Last => MDB_LAST,
-            NextOp::Next => MDB_NEXT,
-            NextOp::Prev => MDB_PREV,
-        }
+        // Test descending from last.
+        check(
+            None,
+            false,
+            vec![
+                b"cc", b"cb", b"ca", b"bc", b"bb", b"ba", b"ac", b"ab", b"aa",
+            ],
+        );
+
+        // Test descending from last using the same cursor again.
+        check(
+            None,
+            false,
+            vec![
+                b"cc", b"cb", b"ca", b"bc", b"bb", b"ba", b"ac", b"ab", b"aa",
+            ],
+        );
+
+        // Test ascending from existing key.
+        let starting_key = b"ba".to_vec();
+        check(
+            Some(&starting_key),
+            true,
+            vec![b"ba", b"bb", b"bc", b"ca", b"cb", b"cc"],
+        );
+
+        // Test descending from existing key.
+        let starting_key = b"bc".to_vec();
+        check(
+            Some(&starting_key),
+            false,
+            vec![b"bc", b"bb", b"ba", b"ac", b"ab", b"aa"],
+        );
+
+        // Test ascending from non-existing key.
+        let starting_key = b"ad".to_vec();
+        check(
+            Some(&starting_key),
+            true,
+            vec![b"ba", b"bb", b"bc", b"ca", b"cb", b"cc"],
+        );
+
+        // Test descending from non-existing key.
+        let starting_key = b"bd".to_vec();
+        check(
+            Some(&starting_key),
+            false,
+            vec![b"bc", b"bb", b"ba", b"ac", b"ab", b"aa"],
+        );
     }
 }
