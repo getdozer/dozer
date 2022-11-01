2e6e58224c75ee359270450115b1f1b3177b2dfd
Snork <matteo.pelati@gmail.com>
2022-11-02 00:14:43 +0800

    refactor: define new source interface

--- dozer-types/src/core/state.rs
+++ dozer-types/src/core/state.rs
@@ -1,5 +1,8 @@
 #![allow(clippy::type_complexity)]
+
+use crate::errors::connector::ConnectorError;
 use crate::errors::database::DatabaseError;
+use crate::types::{Field, OperationEvent, Record, Schema};
 
 pub trait StateStoresManager: Send + Sync {
     fn init_state_store(
@@ -35,4 +38,42 @@ pub trait StateStoreCursor {
     fn next(&mut self) -> Result<bool, DatabaseError>;
     fn prev(&mut self) -> Result<bool, DatabaseError>;
     fn read(&mut self) -> Result<Option<(&[u8], &[u8])>, DatabaseError>;
+}
+
+pub trait Source {
+    fn get_id(&self) -> String;
+    fn get_datasets() -> Vec<String>;
+    fn start() -> Result<(), ConnectorError>;
+    fn stop() -> Result<(), ConnectorError>;
+    fn get_record_reader(&self, dataset: String, snapshot: bool) -> Box<dyn RecordStoreReader>;
+    fn operations_iterator(
+        &self,
+        dataset: String,
+        start_from: u64,
+    ) -> Box<dyn Iterator<Item = OperationEvent> + 'static>;
+}
+
+pub trait RecordStore: RecordStoreReader {
+    fn put_delete_record(
+        &self,
+        key: Vec<Field>,
+        original_seq_no: &[u8],
+    ) -> Result<(), DatabaseError>;
+    fn put_update_record(
+        &self,
+        removed: Vec<Field>,
+        updated: Record,
+        original_seq_no: &[u8],
+    ) -> Result<(), DatabaseError>;
+    fn put_insert_record(&self, added: Record, original_seq_no: &[u8])
+        -> Result<(), DatabaseError>;
+    fn put_schema_update(schema: Schema, original_seq_no: &[u8]) -> Result<(), DatabaseError>;
+}
+
+pub trait RecordStoreReader {
+    fn get_seq_no_range(&self) -> Result<(u64, u64), DatabaseError>;
+    fn get_schema(&self) -> Result<Schema, DatabaseError>;
+    fn get_record(&self, key: Vec<Field>) -> Result<(u64, Record), DatabaseError>;
+    fn records_iterator(&self) -> Box<dyn Iterator<Item = (u64, Record)> + 'static>;
+    fn operations_iterator(&self) -> Box<dyn Iterator<Item = OperationEvent> + 'static>;
 }
