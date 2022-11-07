796c0e58aef019d45d77fb68e77203caffb2e54c
Snork <matteo.pelati@gmail.com>
2022-11-07 17:01:48 +0800

    join initial impl

--- dozer-sql/src/pipeline/relation/relation_index.rs
+++ dozer-sql/src/pipeline/relation/relation_index.rs
@@ -1,56 +1,75 @@
-// use crate::pipeline::expression::execution::Expression;
-// use dozer_core::state::indexer::StateStoreIndexer;
-// use dozer_types::core::node::PortHandle;
-// use dozer_types::errors::pipeline::PipelineError;
-// use dozer_types::types::{Field, FieldDefinition, Operation, Record};
-//
-// pub struct RelationIndex {
-//     id: PortHandle,
-//     indexer: StateStoreIndexer,
-//     pk_indexes: Vec<usize>,
-//     fk_indexes: Vec<(u16, Expression)>,
-//     vals_indexes: Option<Vec<usize>>,
-// }
-//
-// impl RelationIndex {
-//     pub fn new(
-//         id: PortHandle,
-//         indexer: StateStoreIndexer,
-//         pk_indexes: Vec<usize>,
-//         fk_indexes: Vec<(u16, Expression)>,
-//         vals_indexes: Option<Vec<usize>>,
-//     ) -> Self {
-//         Self {
-//             id,
-//             indexer,
-//             pk_indexes,
-//             fk_indexes,
-//             vals_indexes,
-//         }
-//     }
-//
-//     fn extract_fields(&self, in_rec: Record) -> Result<Record, PipelineError> {
-//         Ok(match &self.vals_indexes {
-//             Some(ls) => {
-//                 let mut out_rec: Vec<Field> = Vec::with_capacity(self.vals_indexes.len());
-//                 for i in ls {
-//                     r.push(in_rec.get_value(*i)?);
-//                 }
-//                 Record::new(None, out_rec)
-//             }
-//             _ => r,
-//         })
-//     }
-//
-//     pub fn handle(&self, op: Operation) -> Result<(), PipelineError> {
-//         match op {
-//             Operation::Update { old, new } => {
-//                 out_rec = self.extract_fields(new);
-//             }
-//             Operation::Delete { old } => {}
-//             Operation::Insert { new } => {}
-//         }
-//
-//         Ok(())
-//     }
-// }
+use crate::pipeline::errors::PipelineError;
+use crate::pipeline::expression::execution::{Expression, ExpressionExecutor};
+use dozer_core::dag::node::PortHandle;
+use dozer_core::storage::errors::StorageError::SerializationError;
+use dozer_core::storage::indexed_db::{IndexedDatabase, SecondaryIndexKey};
+use dozer_types::types::{Field, FieldDefinition, Operation, Record};
+
+pub struct RelationIndex {
+    id: PortHandle,
+    db: IndexedDatabase,
+    pk_index: Vec<usize>,
+    fk_indexes: Vec<(u16, Expression)>,
+    vals_indexes: Vec<usize>,
+}
+
+struct FieldData {
+    primary_key: Vec<u8>,
+    foreign_keys: Vec<(u16, Vec<u8>)>,
+    fields: Vec<u8>,
+}
+
+impl RelationIndex {
+    pub fn new(
+        id: PortHandle,
+        db: IndexedDatabase,
+        pk_index: Vec<usize>,
+        fk_indexes: Vec<(u16, Expression)>,
+        vals_indexes: Vec<usize>,
+    ) -> Self {
+        Self {
+            id,
+            db,
+            pk_index,
+            fk_indexes,
+            vals_indexes,
+        }
+    }
+
+    fn extract(&self, rec: &Record) -> Result<FieldData, PipelineError> {
+        let pk_val = rec.get_key(&self.pk_index)?;
+
+        let mut sec_indexes = Vec::<(u16, Vec<u8>)>::with_capacity(self.fk_indexes.len());
+        for fk_idx in &self.fk_indexes {
+            let val = fk_idx.1.evaluate(rec)?.to_bytes()?;
+            sec_indexes.push((fk_idx.0, val));
+        }
+
+        let mut out_fields = Vec::<&Field>::with_capacity(self.vals_indexes.len());
+        for i in &self.vals_indexes {
+            out_fields.push(rec.get_value(i.clone())?);
+        }
+        let out_fields_val = bincode::serialize(&out_fields).map_err(|e| SerializationError {
+            typ: "Vec<Field>".to_string(),
+            reason: Box::new(e),
+        })?;
+
+        Ok(FieldData {
+            primary_key: pk_val,
+            foreign_keys: sec_indexes,
+            fields: out_fields_val,
+        })
+    }
+
+    pub fn handle(&self, op: Operation) -> Result<(), PipelineError> {
+        match op {
+            Operation::Update { old, new } => {}
+            Operation::Delete { old } => {}
+            Operation::Insert { new } => {
+                let fdata = self.extract(&new)?;
+            }
+        }
+
+        Ok(())
+    }
+}
