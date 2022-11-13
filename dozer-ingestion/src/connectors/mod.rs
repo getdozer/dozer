d67140955ef89acdfafc018caeb6036000b12323
Karolis Gudiškis <karolisgud@gmail.com>
2022-11-13 18:15:26 +0200

    Make snowflake feature configurable

--- dozer-ingestion/src/connectors/mod.rs
+++ dozer-ingestion/src/connectors/mod.rs
@@ -5,10 +5,8 @@ pub mod postgres;
 use crate::connectors::postgres::connection::helper::map_connection_config;
 
 use crate::connectors::postgres::connector::{PostgresConfig, PostgresConnector};
-use crate::connectors::snowflake::connector::SnowflakeConnector;
 use crate::errors::ConnectorError;
 use crate::ingestion::Ingestor;
-use dozer_types::ingestion_types::{EthConfig, SnowflakeConfig};
 use dozer_types::log::debug;
 use dozer_types::models::connection::Authentication;
 use dozer_types::models::connection::Connection;
@@ -17,6 +15,16 @@ use dozer_types::serde;
 use dozer_types::serde::{Deserialize, Serialize};
 use dozer_types::types::Schema;
 use std::sync::Arc;
+use dozer_types::ingestion_types::EthConfig;
+
+#[cfg(feature = "snowflake")]
+pub mod snowflake;
+
+#[cfg(feature = "snowflake")]
+use crate::connectors::snowflake::connector::SnowflakeConnector;
+
+#[cfg(feature = "snowflake")]
+use dozer_types::ingestion_types::SnowflakeConfig;
 
 #[cfg(feature = "snowflake")]
 pub mod snowflake;
