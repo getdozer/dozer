4cb60c00fae8a3e6e7e066cebcd80b1cb99556d8
duonganhthu <duonganhthu43@gmail.com>
2022-11-30 13:47:45 +0700

    feat: remove nightly build

--- dozer-admin/build.rs
+++ dozer-admin/build.rs
@@ -1,9 +1,9 @@
 use std::process::Command;
 use std::{env, path::PathBuf};
-#[allow(clippy::all)]
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
+    let mut manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .build_client(false)
@@ -12,20 +12,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .compile(&["protos/api.proto"], &["proto"])
         .unwrap();
     // build dozer-orchestrator
-    //cargo +nightly build -Z unstable-options --manifest-path=Cargo.toml --release --bin dozer --out-dir dozer-admin/dozer-bin
     let orchestrator_cli = Command::new("cargo")
         .args([
-            "+nightly",
             "build",
-            "-Z",
-            "unstable-options",
             "-p",
             "dozer-orchestrator",
             "--release",
             "--bin",
             "dozer",
-            "--out-dir",
-            &env::var("OUT_DIR").unwrap(),
         ])
         .status()
         .unwrap();
@@ -33,6 +27,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         panic!("Cannot build dozer-orchestrator cli");
     }
 
+    //  to go outer path
+    manifest_path.pop();
+    manifest_path.push("target/release/dozer");
+    let dozer_bin_path = manifest_path;
+    Command::new("cp")
+        .args([dozer_bin_path, out_dir])
+        .status()
+        .unwrap();
     // run reset db and run migration
     Command::new("diesel")
         .args(["database", "reset"])
