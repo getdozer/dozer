6327895c36b2c6a8ea700b975163aaf9fe44c294
duonganhthu43 <duonganhthu43@gmail.com>
2022-12-01 10:59:15 +0700

    feat: implement build script for dozer-admin (#295)
    * feat:  build script to bundle orchestrator bin
    
    * chore: run db migration when build
    
    * fix: build scripts
    
    * feat: remove nightly build
    
    * chore: remove all db file

--- dozer-admin/build.rs
+++ dozer-admin/build.rs
@@ -1,14 +1,39 @@
+use std::process::Command;
 use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
+    let mut manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .build_client(false)
         .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
         .file_descriptor_set_path(out_dir.join("dozer_admin_grpc_descriptor.bin"))
         .compile(&["protos/api.proto"], &["proto"])
+        .unwrap();
+    // build dozer-orchestrator
+    let orchestrator_cli = Command::new("cargo")
+        .args([
+            "build",
+            "-p",
+            "dozer-orchestrator",
+            "--release",
+            "--bin",
+            "dozer",
+        ])
+        .status()
+        .unwrap();
+    if !orchestrator_cli.success() {
+        panic!("Cannot build dozer-orchestrator cli");
+    }
+
+    //  to go outer path
+    manifest_path.pop();
+    manifest_path.push("target/release/dozer");
+    let dozer_bin_path = manifest_path;
+    Command::new("cp")
+        .args([dozer_bin_path, out_dir])
+        .status()
         .unwrap();
     Ok(())
 }
