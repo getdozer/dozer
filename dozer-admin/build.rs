3076a7edd8f38dd3aff4ad8e8c38e9f8bb128799
duonganhthu <duonganhthu43@gmail.com>
2022-12-05 11:52:11 +0700

    fix: add param experimental_allow_proto3_optional as default when compiling protoc

--- dozer-admin/build.rs
+++ dozer-admin/build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let mut manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .build_client(false)
         .file_descriptor_set_path(out_dir.join("dozer_admin_grpc_descriptor.bin"))
--- dozer-api/build.rs
+++ dozer-api/build.rs
@@ -1,12 +1,19 @@
 use std::{env, path::PathBuf};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("protos/types.proto")?;
-    tonic_build::compile_protos("protos/api.proto")?;
-    tonic_build::compile_protos("protos/internal.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["protos/types.proto"], &["protos"])?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["protos/api.proto"], &["protos"])?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["protos/internal.proto"], &["protos"])?;
 
     // Sample service generated for tests and development
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("generated_films.bin"))
         .compile(&["protos/films.proto"], &["protos"])?;
 
