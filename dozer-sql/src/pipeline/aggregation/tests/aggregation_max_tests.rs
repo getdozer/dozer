f188538a86e62c5626538295c41849f34a054a44
Chloe Kim <chloeminkyung@gmail.com>
2022-12-06 18:32:44 +0800

    added decimal support for max

--- dozer-sql/src/pipeline/aggregation/tests/aggregation_max_tests.rs
+++ dozer-sql/src/pipeline/aggregation/tests/aggregation_max_tests.rs
@@ -8,6 +8,7 @@ use dozer_types::{
     ordered_float::OrderedFloat,
     types::{Field, FieldDefinition, FieldType, Operation, Record, Schema},
 };
+use dozer_types::rust_decimal::Decimal;
 
 use crate::pipeline::aggregation::tests::aggregation_tests_utils::init_processor;
 
@@ -784,6 +785,415 @@ fn test_max_aggregation_int() {
         old: Record::new(
             None,
             vec![Field::String("Italy".to_string()), Field::Int(100)],
+        ),
+    }];
+    assert_eq!(out, exp);
+}
+
+#[test]
+fn test_max_aggregation_decimal() {
+    let (mut processor, tx) = init_processor(
+        "SELECT Country, MAX(Salary) \
+        FROM Users \
+        WHERE Salary >= 1 GROUP BY Country",
+    )
+        .unwrap();
+
+    let schema = Schema::empty()
+        .field(
+            FieldDefinition::new(String::from("ID"), FieldType::Int, false),
+            false,
+            false,
+        )
+        .field(
+            FieldDefinition::new(String::from("Country"), FieldType::String, false),
+            false,
+            false,
+        )
+        .field(
+            FieldDefinition::new(String::from("Salary"), FieldType::Decimal, false),
+            false,
+            false,
+        )
+        .field(
+            FieldDefinition::new(String::from("MAX(Salary)"), FieldType::Decimal, false),
+            false,
+            false,
+        )
+        .clone();
+
+    let _output_schema = processor
+        .update_schema(
+            DEFAULT_PORT_HANDLE,
+            &HashMap::from([(DEFAULT_PORT_HANDLE, schema)]),
+        )
+        .unwrap();
+
+    // Insert 100 for segment Italy
+    /*
+        Italy, 100.0
+        -------------
+        MIN = 100.0
+    */
+    let inp = Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Insert another 100 for segment Italy
+    /*
+        Italy, 100.0
+        Italy, 100.0
+        -------------
+        MAX = 100.0
+    */
+    let inp = Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    };
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Insert 50 for segment Singapore
+    /*
+        Italy, 100.0
+        Italy, 100.0
+        -------------
+        MAX = 100.0
+
+        Singapore, 50.0
+        -------------
+        MAX = 50.0
+    */
+    let inp = Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Singapore".to_string()),
+                Field::Decimal(Decimal::new(50, 0)),
+                Field::Decimal(Decimal::new(50, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Singapore".to_string()),
+                Field::Decimal(Decimal::new(50, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Update Singapore segment to Italy
+    /*
+        Italy, 100.0
+        Italy, 100.0
+        Italy, 50.0
+        -------------
+        MAX = 100.0
+    */
+    let inp = Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Singapore".to_string()),
+                Field::Decimal(Decimal::new(50, 0)),
+                Field::Decimal(Decimal::new(50, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(50, 0)),
+                Field::Decimal(Decimal::new(50, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![
+        Operation::Update {
+            old: Record::new(
+                None,
+                vec![
+                    Field::String("Italy".to_string()),
+                    Field::Decimal(Decimal::new(100, 0)),
+                ],
+            ),
+            new: Record::new(
+                None,
+                vec![
+                    Field::String("Italy".to_string()),
+                    Field::Decimal(Decimal::new(100, 0)),
+                ],
+            ),
+        },
+        Operation::Delete {
+            old: Record::new(
+                None,
+                vec![
+                    Field::String("Singapore".to_string()),
+                    Field::Decimal(Decimal::new(50, 0)),
+                ],
+            ),
+        },
+    ];
+    assert_eq!(out, exp);
+
+    // Update Italy value 100 -> 200
+    /*
+        Italy, 200.0
+        Italy, 100.0
+        Italy, 50.0
+        -------------
+        MAX = 200.0
+    */
+    let inp = Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(200, 0)),
+                Field::Decimal(Decimal::new(200, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(200, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Delete 1 record (200)
+    /*
+        Italy, 100.0
+        Italy, 50.0
+        -------------
+        MAX = 100.0
+    */
+    let inp = Operation::Delete {
+        old: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(200, 0)),
+                Field::Decimal(Decimal::new(200, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(200, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Delete another record (50)
+    /*
+        Italy, 100.0
+        -------------
+        MAX = 100.0
+    */
+    let inp = Operation::Delete {
+        old: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(50, 0)),
+                Field::Decimal(Decimal::new(50, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Update {
+        old: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+        new: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    }];
+    assert_eq!(out, exp);
+
+    // Delete last record
+    /*
+        -------------
+        MAX = 0.0
+    */
+    let inp = Operation::Delete {
+        old: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
+        ),
+    };
+
+    let out = processor
+        .aggregate(
+            &mut SharedTransaction::new(&tx),
+            &processor.db.clone().unwrap(),
+            inp,
+        )
+        .unwrap_or_else(|_e| panic!("Error executing aggregate"));
+
+    let exp = vec![Operation::Delete {
+        old: Record::new(
+            None,
+            vec![
+                Field::String("Italy".to_string()),
+                Field::Decimal(Decimal::new(100, 0)),
+            ],
         ),
     }];
     assert_eq!(out, exp);
