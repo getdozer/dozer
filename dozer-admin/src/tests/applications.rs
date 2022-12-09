2280d1263d13b934f808839551f1a0c34e0caf51
duonganhthu43 <duonganhthu43@gmail.com>
2022-12-09 11:28:37 +0700

    chore: Implement tests in dozer admin (#328)
    * chore: temp trigger test-coverage
    
    * chore: add endpoints testcase
    
    * feat: add test connection
    
    * fix: remove duplicate
    
    * chore: remove unused code
    
    * chore: update mock db type
    
    * chore: add kafka, events to proto and test
    
    * fix: clippy fmt
    
    * fix: format
    
    * chore: move all authentication_types in one file

--- dozer-admin/src/tests/applications.rs
+++ dozer-admin/src/tests/applications.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod grpc_service {
     use crate::server::dozer_admin_grpc::{
-        CreateAppRequest, CreateAppResponse, ListAppRequest, ListAppResponse,
+        CreateAppRequest, CreateAppResponse, ListAppRequest, ListAppResponse, StartPipelineRequest,
+        StartPipelineResponse, UpdateAppRequest, UpdateAppResponse,
     };
     use crate::services::application_service::AppService;
     use crate::tests::util_sqlite_setup::database_url_for_test_env;
@@ -32,5 +33,34 @@ mod grpc_service {
         };
         let result: CreateAppResponse = application_service.create(request.to_owned()).unwrap();
         assert_eq!(result.data.unwrap().name, request.app_name);
+    }
+
+    #[test]
+    pub fn update() {
+        let test_db_connection = database_url_for_test_env();
+        let db_pool = establish_test_connection(test_db_connection);
+        let application_service = AppService::new(db_pool);
+        let setup_ids = get_setup_ids();
+
+        let request = UpdateAppRequest {
+            app_id: setup_ids.app_id,
+            name: "updated_app_name".to_owned(),
+        };
+        let result: UpdateAppResponse = application_service.update_app(request.to_owned()).unwrap();
+        assert_eq!(result.data.unwrap().name, request.name);
+    }
+
+    #[test]
+    pub fn trigger_cli() {
+        let test_db_connection = database_url_for_test_env();
+        let db_pool = establish_test_connection(test_db_connection);
+        let application_service = AppService::new(db_pool);
+        let setup_ids = get_setup_ids();
+
+        let request = StartPipelineRequest {
+            app_id: setup_ids.app_id,
+        };
+        let result: StartPipelineResponse = application_service.start_pipeline(request).unwrap();
+        assert!(result.success);
     }
 }
