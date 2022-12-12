fdbd911879ebeb900e94aeccd774351a3ccd1430
Karolis Gudiškis <karolisgud@gmail.com>
2022-12-12 16:42:00 +0200

    Add PK fetch from schema registry

--- dozer-ingestion/src/connectors/kafka/debezium/schema_registry.rs
+++ dozer-ingestion/src/connectors/kafka/debezium/schema_registry.rs
@@ -1,9 +1,9 @@
 use crate::connectors::kafka::debezium::schema::SchemaFetcher;
 use crate::connectors::kafka::debezium::stream_consumer::DebeziumSchemaStruct;
 use crate::connectors::TableInfo;
-use crate::errors::DebeziumError::JsonDecodeError;
+use crate::errors::DebeziumError::{JsonDecodeError, SchemaRegistryFetchError};
 use crate::errors::DebeziumSchemaError::TypeNotSupported;
-use crate::errors::{ConnectorError, DebeziumSchemaError};
+use crate::errors::{ConnectorError, DebeziumError, DebeziumSchemaError};
 use dozer_types::ingestion_types::KafkaConfig;
 use dozer_types::serde_json;
 use dozer_types::serde_json::Value;
@@ -41,8 +41,9 @@ pub fn map_typ(schema: &DebeziumSchemaStruct) -> Result<(FieldType, bool), Debez
                 }
             }
 
-            Err(DebeziumSchemaError::TypeNotSupported("Array".to_string()))
+            Err(TypeNotSupported("Array".to_string()))
         }
+
         Value::Object(obj) => map_typ(&DebeziumSchemaStruct {
             r#type: obj.get("type").unwrap().clone(),
             fields: None,
@@ -56,28 +57,43 @@ pub fn map_typ(schema: &DebeziumSchemaStruct) -> Result<(FieldType, bool), Debez
     }
 }
 
+impl SchemaRegistry {
+    pub fn fetch_struct(
+        sr_settings: &SrSettings,
+        table_name: &str,
+        is_key: bool,
+    ) -> Result<DebeziumSchemaStruct, DebeziumError> {
+        let schema_result =
+            schema_registry_converter::blocking::schema_registry::get_schema_by_subject(
+                sr_settings,
+                &SubjectNameStrategy::TopicNameStrategy(table_name.to_string(), is_key),
+            )
+            .map_err(SchemaRegistryFetchError)?;
+
+        serde_json::from_str::<DebeziumSchemaStruct>(&schema_result.schema).map_err(JsonDecodeError)
+    }
+}
+
 impl SchemaFetcher for SchemaRegistry {
     fn get_schema(
         table_names: Option<Vec<TableInfo>>,
         config: KafkaConfig,
-    ) -> Result<Vec<(String, dozer_types::types::Schema)>, ConnectorError> {
+    ) -> Result<Vec<(String, Schema)>, ConnectorError> {
         let sr_settings = SrSettings::new(config.schema_registry_url.unwrap());
         table_names.map_or(Ok(vec![]), |tables| {
             tables.get(0).map_or(Ok(vec![]), |table| {
-                let schema_result =
-                    schema_registry_converter::blocking::schema_registry::get_schema_by_subject(
-                        &sr_settings,
-                        &SubjectNameStrategy::TopicNameStrategy(table.clone().name, false),
-                    )
-                    .unwrap();
+                let key_result = SchemaRegistry::fetch_struct(&sr_settings, &table.name, true)?;
+                let schema_result = SchemaRegistry::fetch_struct(&sr_settings, &table.name, false)?;
 
-                let result: DebeziumSchemaStruct =
-                    serde_json::from_str(&schema_result.schema).map_err(JsonDecodeError)?;
+                let pk_fields = key_result.fields.map_or(vec![], |fields| {
+                    fields
+                        .iter()
+                        .map(|f| f.name.clone().map_or("".to_string(), |name| name))
+                        .collect()
+                });
 
-                let mut schema_data: Option<
-                    Result<Vec<(String, dozer_types::types::Schema)>, ConnectorError>,
-                > = None;
-                result.fields.iter().for_each(|field| {
+                let mut schema_data: Option<Result<Vec<(String, Schema)>, ConnectorError>> = None;
+                schema_result.fields.iter().for_each(|field| {
                     field.iter().for_each(|f| {
                         if f.name.clone().unwrap() == "before" {
                             for typ in f.r#type.as_array().unwrap() {
@@ -85,7 +101,7 @@ impl SchemaFetcher for SchemaRegistry {
                                     let fields_value = obj.get("fields").unwrap();
                                     let fields_value_struct: Vec<DebeziumSchemaStruct> =
                                         serde_json::from_value(fields_value.clone()).unwrap();
-                                    // let mut pk_keys_indexes = vec![];
+                                    let mut pk_keys_indexes = vec![];
                                     let mut fields_schema_map: HashMap<
                                         String,
                                         &DebeziumSchemaStruct,
@@ -97,12 +113,12 @@ impl SchemaFetcher for SchemaRegistry {
                                     > = fields_value_struct
                                         .iter()
                                         .enumerate()
-                                        .map(|(_idx, f)| {
+                                        .map(|(idx, f)| {
                                             let (typ, nullable) = map_typ(f)?;
                                             let name = f.name.clone().unwrap();
-                                            // if pk_fields.contains(&name) {
-                                            //     pk_keys_indexes.push(idx);
-                                            // }
+                                            if pk_fields.contains(&name) {
+                                                pk_keys_indexes.push(idx);
+                                            }
                                             fields_schema_map.insert(name.clone(), f);
                                             Ok(FieldDefinition {
                                                 name,
@@ -116,7 +132,7 @@ impl SchemaFetcher for SchemaRegistry {
                                         identifier: Some(SchemaIdentifier { id: 1, version: 1 }),
                                         fields: defined_fields.unwrap(),
                                         values: vec![],
-                                        primary_index: vec![],
+                                        primary_index: pk_keys_indexes,
                                     };
 
                                     schema_data = Some(Ok(vec![(table.name.clone(), schema)]));
