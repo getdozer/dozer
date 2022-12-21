e83b721233fca926a4d11c4e9305a9271a7cb780
VG <vivekg342@gmail.com>
2022-12-21 12:57:34 +0800

    chore: update eth sample

--- dozer-ingestion/tests/connectors/simple_iterator_samples/src/eth.rs
+++ dozer-ingestion/tests/connectors/simple_iterator_samples/src/eth.rs
@@ -6,20 +6,21 @@ use dozer_ingestion::{
 use dozer_types::{
     ingestion_types::{EthConfig, EthFilter},
     log::info,
-    log4rs,
 };
 use std::thread;
 fn main() -> Result<(), ConnectorError> {
-    log4rs::init_file("./log4rs.yaml", Default::default())
-        .unwrap_or_else(|_e| panic!("Unable to find log4rs config file"));
-    let wss_url = "wss://localhost:8545".to_string();
-    let (ingestor, mut iterator) = Ingestor::initialize_channel(IngestionConfig::default());
+    dozer_tracing::init_telemetry(false).unwrap();
+
+    // SET ENV variable `ETH_WSS_URL`
+    let wss_url = std::env::var("ETH_WSS_URL").unwrap_or("wss://localhost:8545".to_string());
+
+    let (ingestor, iterator) = Ingestor::initialize_channel(IngestionConfig::default());
     let t = thread::spawn(move || -> Result<(), ConnectorError> {
         let mut eth_connector = EthConnector::new(
             1,
             EthConfig {
                 name: "all_eth_events".to_string(),
-                filter: EthFilter::default(),
+                filter: Some(EthFilter::default()),
                 wss_url,
             },
         );
@@ -28,7 +29,7 @@ fn main() -> Result<(), ConnectorError> {
     });
 
     loop {
-        let msg = iterator.next();
+        let msg = iterator.write().next();
 
         if let Some(msg) = msg {
             info!("{:?}", msg);
