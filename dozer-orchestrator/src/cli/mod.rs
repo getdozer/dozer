55638d972cc7742d23164d33700d02b38891223f
VG <vivekg342@gmail.com>
2022-12-28 01:14:21 +0800

    chore: inclue env vars in dozer config

--- dozer-orchestrator/src/cli/mod.rs
+++ dozer-orchestrator/src/cli/mod.rs
@@ -3,12 +3,30 @@ pub mod tests;
 pub mod types;
 use crate::errors::CliError;
 use dozer_types::{models::app_config::Config, serde_yaml};
-use std::fs;
+use handlebars::Handlebars;
+use std::{collections::BTreeMap, fs};
+
 pub fn load_config(config_path: String) -> Result<Config, CliError> {
     let contents = fs::read_to_string(config_path.clone())
         .map_err(|_| CliError::FailedToLoadFile(config_path))?;
+
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_string("config", contents)
+        .map_err(|e| CliError::FailedToParseYaml(Box::new(e)))?;
+
+    let mut data = BTreeMap::new();
+
+    for (key, value) in std::env::vars() {
+        data.insert(key, value);
+    }
+
+    let config_str = handlebars
+        .render("config", &data)
+        .map_err(|e| CliError::FailedToParseYaml(Box::new(e)))?;
+
     let config =
-        serde_yaml::from_str(&contents).map_err(|e| CliError::FailedToParseYaml(Box::new(e)))?;
+        serde_yaml::from_str(&config_str).map_err(|e| CliError::FailedToParseYaml(Box::new(e)))?;
     Ok(config)
 }
 
