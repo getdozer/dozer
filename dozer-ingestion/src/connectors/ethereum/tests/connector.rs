2d18f793b84497b64788ead7754727a6d2add1c7
VG <vivekg342@gmail.com>
2022-12-28 14:07:04 +0800

    feat: implement eth sample and tests (#470)
    * adding new user facing docker setup for examples
    
    * fixed example 1
    
    * fixed example 1
    
    * fixed example 1
    
    * chore: reogranize docker release command
    
    * chore: reorganize sample folder structure
    
    * feat: init eth sample
    
    * chore: stage eth bug
    
    * chore: inclue env vars in dozer config
    
    * chore: implement eth test case
    
    * chore: fix .env
    
    * chore: fix clippy
    
    * chore: include a note in README about m1
    
    * chore: handle connector erors
    
    * chore: comment broken links
    
    Co-authored-by: Chloe Kim <chloeminkyung@gmail.com>

---
+++ dozer-ingestion/src/connectors/ethereum/tests/connector.rs
@@ -0,0 +1,34 @@
+use dozer_types::types::{Field, Operation};
+use hex_literal::hex;
+
+use super::helper::run_eth_sample;
+
+#[test]
+#[ignore]
+fn test_eth_iterator() {
+    let wss_url = "ws://localhost:8545".to_string();
+    let my_account = hex!("b49B3BEE604eF76410E84C7C98bC20335FdA0f75").into();
+
+    let validate = |op: &Operation, idx: usize, field: Option<Field>| {
+        if let Operation::Insert { new } = op {
+            assert_eq!(new.values.get(idx), field.as_ref());
+        } else {
+            panic!("expected insert");
+        }
+    };
+
+    let (contract, msgs) = run_eth_sample(wss_url, my_account);
+
+    let address = format!("{:?}", contract.address());
+    validate(&msgs[0], 1, Some(Field::String(address)));
+
+    validate(
+        &msgs[1],
+        0,
+        Some(Field::String(format!("{:?}", my_account))),
+    );
+
+    validate(&msgs[1], 1, Some(Field::String("Hello World!".to_string())));
+
+    validate(&msgs[2], 0, None);
+}
