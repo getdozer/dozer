027905a7127058d2748f62ce3d612b9c4acb4ca1
chubei <914745487@qq.com>
2022-12-30 19:09:10 +0800

    test: Add receiver loop unit tests

--- dozer-core/src/dag/executor/receiver_loop.rs
+++ dozer-core/src/dag/executor/receiver_loop.rs
@@ -12,7 +12,7 @@ use crate::dag::{
 
 use super::{name::Name, ExecutorOperation, InputPortState};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum MappedExecutorOperation {
     Data { op: Operation },
     Commit { epoch: Epoch },
@@ -96,5 +96,206 @@ pub trait ReceiverLoop: Name {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::swap;
+
+    use crossbeam::channel::{unbounded, Sender};
+    use dozer_types::types::{Field, Record};
+
+    use crate::dag::node::NodeHandle;
+
+    use super::*;
+
+    #[test]
+    fn test_map_executor_operation() {
+        let old = Record::new(None, vec![Field::Int(1)]);
+        let new = Record::new(None, vec![Field::Int(2)]);
+        let epoch = Epoch::new(1, HashMap::new());
+        assert_eq!(
+            map_executor_operation(ExecutorOperation::Insert { new: new.clone() }),
+            MappedExecutorOperation::Data {
+                op: Operation::Insert { new: new.clone() }
+            }
+        );
+        assert_eq!(
+            map_executor_operation(ExecutorOperation::Update {
+                old: old.clone(),
+                new: new.clone()
+            }),
+            MappedExecutorOperation::Data {
+                op: Operation::Update {
+                    old: old.clone(),
+                    new
+                }
+            }
+        );
+        assert_eq!(
+            map_executor_operation(ExecutorOperation::Delete { old: old.clone() }),
+            MappedExecutorOperation::Data {
+                op: Operation::Delete { old }
+            }
+        );
+        assert_eq!(
+            map_executor_operation(ExecutorOperation::Commit {
+                epoch_details: epoch.clone()
+            }),
+            MappedExecutorOperation::Commit { epoch }
+        );
+        assert_eq!(
+            map_executor_operation(ExecutorOperation::Terminate),
+            MappedExecutorOperation::Terminate
+        );
+    }
+
+    struct TestReceiverLoop {
+        receivers: Vec<Receiver<ExecutorOperation>>,
+        ops: Vec<(usize, Operation)>,
+        commits: Vec<Epoch>,
+        num_termations: usize,
+    }
+
+    impl Name for TestReceiverLoop {
+        fn name(&self) -> Cow<str> {
+            Cow::Borrowed("TestReceiverLoop")
+        }
+    }
+
+    impl ReceiverLoop for TestReceiverLoop {
+        fn receivers(&mut self) -> Vec<Receiver<ExecutorOperation>> {
+            let mut result = vec![];
+            swap(&mut self.receivers, &mut result);
+            result
+        }
+
+        fn receiver_name(&self, index: usize) -> Cow<str> {
+            Cow::Owned(format!("receiver_{}", index))
+        }
+
+        fn on_op(&mut self, index: usize, op: Operation) -> Result<(), ExecutionError> {
+            self.ops.push((index, op));
+            Ok(())
+        }
+
+        fn on_commit(&mut self, epoch: &Epoch) -> Result<(), ExecutionError> {
+            self.commits.push(epoch.clone());
+            Ok(())
+        }
+
+        fn on_terminate(&mut self) -> Result<(), ExecutionError> {
+            self.num_termations += 1;
+            Ok(())
+        }
+    }
+
+    impl TestReceiverLoop {
+        fn new(num_receivers: usize) -> (TestReceiverLoop, Vec<Sender<ExecutorOperation>>) {
+            let (senders, receivers) = (0..num_receivers).map(|_| unbounded()).unzip();
+            (
+                TestReceiverLoop {
+                    receivers,
+                    ops: vec![],
+                    commits: vec![],
+                    num_termations: 0,
+                },
+                senders,
+            )
+        }
+    }
+
+    #[test]
+    fn receiver_loop_stops_on_terminate() {
+        let (mut test_loop, senders) = TestReceiverLoop::new(2);
+        senders[0].send(ExecutorOperation::Terminate).unwrap();
+        senders[1].send(ExecutorOperation::Terminate).unwrap();
+        test_loop.receiver_loop().unwrap();
+        assert_eq!(test_loop.num_termations, 1);
+    }
+
+    #[test]
+    fn receiver_loop_forwards_op() {
+        let (mut test_loop, senders) = TestReceiverLoop::new(2);
+        let record = Record::new(None, vec![Field::Int(1)]);
+        senders[0]
+            .send(ExecutorOperation::Insert {
+                new: record.clone(),
+            })
+            .unwrap();
+        senders[0].send(ExecutorOperation::Terminate).unwrap();
+        senders[1].send(ExecutorOperation::Terminate).unwrap();
+        test_loop.receiver_loop().unwrap();
+        assert_eq!(test_loop.ops, vec![(0, Operation::Insert { new: record })]);
+    }
+
+    #[test]
+    fn receiver_loop_merges_commit_epoch_and_increases_epoch_id() {
+        let (mut test_loop, senders) = TestReceiverLoop::new(2);
+        let mut details = HashMap::new();
+        details.insert(NodeHandle::new(None, "0".to_string()), (0, 0));
+        let mut epoch0 = Epoch::new(0, details);
+        let mut details = HashMap::new();
+        details.insert(NodeHandle::new(None, "1".to_string()), (0, 0));
+        let mut epoch1 = Epoch::new(0, details);
+        senders[0]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch0.clone(),
+            })
+            .unwrap();
+        senders[1]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch1.clone(),
+            })
+            .unwrap();
+        epoch0.id = 1;
+        epoch1.id = 1;
+        senders[0]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch0.clone(),
+            })
+            .unwrap();
+        senders[1]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch1.clone(),
+            })
+            .unwrap();
+        senders[0].send(ExecutorOperation::Terminate).unwrap();
+        senders[1].send(ExecutorOperation::Terminate).unwrap();
+        test_loop.receiver_loop().unwrap();
+
+        let mut details = HashMap::new();
+        details.extend(epoch0.details);
+        details.extend(epoch1.details);
+        assert_eq!(
+            test_loop.commits,
+            vec![Epoch::new(0, details.clone()), Epoch::new(1, details)]
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn receiver_loop_panics_on_inconsistent_commit_epoch() {
+        let (mut test_loop, senders) = TestReceiverLoop::new(2);
+        let mut details = HashMap::new();
+        details.insert(NodeHandle::new(None, "0".to_string()), (0, 0));
+        let epoch0 = Epoch::new(0, details);
+        let mut details = HashMap::new();
+        details.insert(NodeHandle::new(None, "1".to_string()), (0, 0));
+        let epoch1 = Epoch::new(1, details);
+        senders[0]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch0.clone(),
+            })
+            .unwrap();
+        senders[1]
+            .send(ExecutorOperation::Commit {
+                epoch_details: epoch1.clone(),
+            })
+            .unwrap();
+        senders[0].send(ExecutorOperation::Terminate).unwrap();
+        senders[1].send(ExecutorOperation::Terminate).unwrap();
+        test_loop.receiver_loop().unwrap();
     }
 }
