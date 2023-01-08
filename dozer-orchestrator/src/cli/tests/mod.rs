059c7e5c98521d20defe16443d63602d0efcbedb
VG <vivekg342@gmail.com>
2023-01-09 02:06:59 +0800

    chore: remove unused test case

--- dozer-orchestrator/src/cli/tests/mod.rs
+++ dozer-orchestrator/src/cli/tests/mod.rs
@@ -1,30 +1,3 @@
 mod deserialize;
 mod helper;
 mod validation;
-
-use rustyline::validate::MatchingBracketValidator;
-use rustyline::{Cmd, Editor, EventHandler, KeyCode, KeyEvent, Modifiers, Result};
-use rustyline_derive::{Completer, Helper, Highlighter, Hinter, Validator};
-
-#[derive(Completer, Helper, Highlighter, Hinter, Validator)]
-struct InputValidator {
-    #[rustyline(Validator)]
-    brackets: MatchingBracketValidator,
-}
-#[test]
-fn test_multiline() -> Result<()> {
-    let h = InputValidator {
-        brackets: MatchingBracketValidator::new(),
-    };
-    let mut rl = Editor::new()?;
-    rl.set_helper(Some(h));
-    rl.bind_sequence(
-        KeyEvent(KeyCode::Char('s'), Modifiers::CTRL),
-        EventHandler::Simple(Cmd::Newline),
-    );
-
-    let input = rl.readline("> ")?;
-    println!("Input: {input}");
-
-    Ok(())
-}
