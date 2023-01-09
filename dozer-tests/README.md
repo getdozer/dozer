b0b148656ffe329a7c6eff03e16a10f09d90cc7a
Bei Chu <914745487@qq.com>
2023-01-10 01:31:10 +0800

    test: e2e: Rewrite framework to use prebuilt docker container (#567)
    * test: e2e: Rewrite framework to use prebuilt docker container
    
    * chore: add postgres testcase
    
    * chore: Rename source to connection
    
    Co-authored-by: duonganhthu <duonganhthu43@gmail.com>

---
+++ dozer-tests/README.md
@@ -0,0 +1,76 @@
+# Dozer Tests
+
+This crate contains the end-to-end test framework and the test client used in the framework (and some unit tests, which are not covered in this README).
+
+## Run Tests Locally
+
+To run the e2e tests locally, first build the debug dozer binary:
+
+```rust
+cargo build --bin dozer
+```
+
+The test framework expects the binary located at `target/debug/dozer`. You can override that by setting `DOZER_BIN` environment variable.
+
+Then run the `dozer-tests` binary:
+
+```rust
+cargo run --bin dozer-tests
+```
+
+It will run all the test cases and stop on first failure.
+
+Filtering of test cases similar to `cargo test` is supported. For example, to run all test cases whose names start with `eth`:
+
+```rust
+cargo run --bin dozer-tests -- eth
+```
+
+The `--` before `eth` is how cargo knows following arguments are for the binary.
+
+### Expected Environment Variables for Running the Tests
+
+- ETH_WSS_URL for Ethereum connector.
+
+## Add a Test Case
+
+All test cases are located under `dozer-tests/src/e2e_tests/cases`. Each case input is within a separate directory, the directory name being the test case name.
+
+Each test case should have a `dozer-config.yaml` file, which `dozer` runs with.
+
+If `expectations.json` is found in the case directory, the framework expects `dozers` to start successfully with the given config file, and it will check if all the expectations are met.
+
+For all supported expectation checks, see `expectations.rs`.
+
+Otherwise, if `error.json` is found, the framework expects `dozer` fails to start with the given config file, and it will check if `dozer` fails with the expected error.
+
+For all supported error expectation checks, see `expectations.rs`.
+
+## Add a Connection
+
+Test cases may need to establish various kinds of connections. Connection services can run as a docker compose service.
+
+The framework traverses `dozer-config.yaml`, and for every `connection`, it tries to find a directory with the connection name under `dozer-tests/src/e2e_tests/connections`. If found, it adds the connection service defined in that directory to a docker compose file, and starts the containers before running the test client.
+
+The connection directory must have a `Dockerfile` used for building the image. The build context will be the connection directory.
+
+It can optionally contain a `service.yaml` file, whose content will be added to the connection service section of the docker commpose file. The `build` section of `service.yaml` will be overwritten. The working directory of the `docker compose` run will be the repository root, so be careful with relative paths in `service.yaml` (better don't use them).
+
+### Troubleshoot Connections
+
+- If `service.yaml` parsing fails or generated `docker-compose.yaml` misses some content from it, it's probably because our data model doesn't agree with the file content. You can check the data model at `docker_compose.rs`.
+
+## Run Tests Like in CI
+
+```rust
+cargo build --bin dozer-test-client
+cargo run --bin dozer-tests -- -r buildkite
+```
+
+If `buildkite` runner type is used, the framework will not use local `dozer` binary or run test client in-process. Instead, it creates `docker-compose.yaml` files for the test cases, which contains all the connection services, the `dozer` service and the test client. A separate `docker compose` process is started to validate all the expectations using the client.
+
+This mode requires you to build `dozer-test-client` binary first, and the artifact must be located at `target/debug/dozer-test-client`.
+
+### Troubleshoot Buildkite Runner
+
+- If `dozer-test-client` service says `exec /dozer-test-client: exec format error`, it's because the container is linux x86_64, but your dev machine is not.
--- dozer-tests/src/e2e_tests/checker/client.rs
+++ dozer-tests/src/e2e_tests/checker/client.rs
@@ -1,5 +1,3 @@
-use std::{net::SocketAddr, str::FromStr};
-
 use dozer_api::{
     grpc::{
         common_grpc::{
@@ -19,11 +17,11 @@ use dozer_types::{
     types::{FieldDefinition, FieldType, DATE_FORMAT},
 };
 
-use super::expectation::{EndpointExpectation, Expectation};
+use super::super::expectation::{EndpointExpectation, Expectation};
 
 pub struct Client {
     config: Config,
-    rest_endpoint: SocketAddr,
+    rest_endpoint: String,
     rest_client: reqwest::Client,
     health_grpc_client: HealthGrpcServiceClient<Channel>,
     common_grpc_client: CommonGrpcServiceClient<Channel>,
@@ -34,26 +32,29 @@ impl Client {
         let api = config.api.clone().unwrap_or_default();
 
         let rest = api.rest.unwrap_or_default();
-        let rest_endpoint = SocketAddr::from_str(&format!("{}:{}", rest.host, rest.port))
-            .expect(&format!("Bad rest endpoint: {}:{}", rest.host, rest.port));
+        let rest_endpoint = format!("http://{}:{}", rest.host, rest.port);
 
         let grpc = api.grpc.unwrap_or_default();
         let grpc_endpoint_string = format!("http://{}:{}", grpc.host, grpc.port);
         let grpc_endpoint = Endpoint::from_shared(grpc_endpoint_string.clone())
-            .expect(&format!("Invalid grpc endpoint {}", grpc_endpoint_string));
+            .unwrap_or_else(|e| panic!("Invalid grpc endpoint {}: {}", grpc_endpoint_string, e));
 
         let health_grpc_client = HealthGrpcServiceClient::connect(grpc_endpoint.clone())
             .await
-            .expect(&format!(
-                "Health grpc client cannot connect to endpoint {}",
-                grpc_endpoint_string
-            ));
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Health grpc client cannot connect to endpoint {}: {}",
+                    grpc_endpoint_string, e
+                )
+            });
         let common_grpc_client = CommonGrpcServiceClient::connect(grpc_endpoint.clone())
             .await
-            .expect(&format!(
-                "Common grpc client cannot connect to endpoint {}",
-                grpc_endpoint_string
-            ));
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Common grpc client cannot connect to endpoint {}: {}",
+                    grpc_endpoint_string, e
+                )
+            });
 
         Self {
             config,
@@ -76,7 +77,7 @@ impl Client {
                     .endpoints
                     .iter()
                     .find(|e| &e.name == endpoint)
-                    .expect(&format!("Cannot find endpoint {} in config", endpoint))
+                    .unwrap_or_else(|| panic!("Cannot find endpoint {} in config", endpoint))
                     .path
                     .clone();
                 self.check_endpoint_existence(endpoint, &rest_path).await;
@@ -92,7 +93,7 @@ impl Client {
         // REST health.
         let response = self
             .rest_client
-            .get(&format!("http://{}/health", self.rest_endpoint))
+            .get(&format!("{}/health", self.rest_endpoint))
             .send()
             .await
             .expect("Cannot get response from rest health endpoint");
@@ -116,13 +117,15 @@ impl Client {
         // REST endpoint oapi.
         let response = self
             .rest_client
-            .post(&format!("http://{}{}/oapi", self.rest_endpoint, rest_path))
+            .post(&format!("{}{}/oapi", self.rest_endpoint, rest_path))
             .send()
             .await
-            .expect(&format!(
-                "Cannot get oapi response from rest endpoint {}, path is {}",
-                endpoint, rest_path
-            ));
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Cannot get oapi response from rest endpoint {}, path is {}: {}",
+                    endpoint, rest_path, e
+                )
+            });
         let status = response.status();
         if !status.is_success() {
             panic!(
@@ -171,13 +174,15 @@ impl Client {
         // REST OpenAPI schema.
         let response = self
             .rest_client
-            .post(&format!("http://{}{}/oapi", self.rest_endpoint, rest_path))
+            .post(&format!("{}{}/oapi", self.rest_endpoint, rest_path))
             .send()
             .await
-            .expect(&format!(
-                "Cannot get oapi response from rest endpoint {}, path is {}",
-                endpoint, rest_path
-            ));
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Cannot get oapi response from rest endpoint {}, path is {}: {}",
+                    endpoint, rest_path, e
+                )
+            });
         let status = response.status();
         if !status.is_success() {
             panic!(
@@ -185,27 +190,35 @@ impl Client {
                 endpoint, status, rest_path
             );
         }
-        let open_api: OpenAPI = response.json().await.expect(&format!(
-            "Cannot parse oapi response from rest endpoint {}, path is {}",
-            endpoint, rest_path,
-        ));
+        let open_api: OpenAPI = response.json().await.unwrap_or_else(|e| {
+            panic!(
+                "Cannot parse oapi response from rest endpoint {}, path is {}: {}",
+                endpoint, rest_path, e
+            )
+        });
         let schema = open_api
             .components
             .as_ref()
-            .expect(&format!(
-                "Cannot find components in oapi response from rest endpoint {}, path is {}",
-                endpoint, rest_path,
-            ))
+            .unwrap_or_else(|| {
+                panic!(
+                    "Cannot find components in oapi response from rest endpoint {}, path is {}",
+                    endpoint, rest_path
+                )
+            })
             .schemas
             .get(endpoint)
-            .expect(&format!(
-                "Cannot find schema for endpoint {} in oapi response, path is {}",
-                endpoint, rest_path,
-            ));
-        let schema = schema.as_item().expect(&format!(
-            "Expecting schema item for endpoint {} in oapi response, path is {}",
-            endpoint, rest_path
-        ));
+            .unwrap_or_else(|| {
+                panic!(
+                    "Cannot find schema for endpoint {} in oapi response, path is {}",
+                    endpoint, rest_path
+                )
+            });
+        let schema = schema.as_item().unwrap_or_else(|| {
+            panic!(
+                "Expecting schema item for endpoint {} in oapi response, path is {}",
+                endpoint, rest_path
+            )
+        });
         let (properties, required) = match &schema.schema_kind {
             SchemaKind::Type(dozer_api::openapiv3::Type::Object(object_type)) => {
                 (&object_type.properties, &object_type.required)
@@ -229,10 +242,12 @@ impl Client {
                 "Check REST schema failed for endpoint {}, expected field name {}, got {}",
                 endpoint, field.name, property.0
             );
-            let schema = property.1.as_item().expect(&format!(
-                "Expecting schema item for endpoint {}, field {} in oapi response, path is {}",
-                endpoint, field.name, rest_path
-            ));
+            let schema = property.1.as_item().unwrap_or_else(|| {
+                panic!(
+                    "Expecting schema item for endpoint {}, field {} in oapi response, path is {}",
+                    endpoint, field.name, rest_path
+                )
+            });
             let oapi_type = match &schema.schema_kind {
                 SchemaKind::Type(oapi_type) => oapi_type,
                 _ => panic!(
@@ -261,7 +276,7 @@ impl Client {
                 endpoint: endpoint.to_string(),
             })
             .await
-            .expect(&format!("Cannot get fields of endpoint {}", endpoint))
+            .unwrap_or_else(|e| panic!("Cannot get fields of endpoint {}: {}", endpoint, e))
             .into_inner()
             .fields;
         assert_eq!(
@@ -287,9 +302,13 @@ impl Client {
                 actual_field.typ
             );
             assert_eq!(
-                actual_field.nullable, field.nullable,
+                actual_field.nullable,
+                field.nullable,
                 "Check common gRPC schema failed for endpoint {}, field {}, expected field nullable {}, got {}",
-                field.name, endpoint, field.nullable, actual_field.nullable
+                endpoint,
+                field.name,
+                field.nullable,
+                actual_field.nullable
             );
         }
 
@@ -303,10 +322,12 @@ async fn check_grpc_health(client: &mut HealthGrpcServiceClient<Channel>, servic
             service: service.clone(),
         })
         .await
-        .expect(&format!(
-            "Cannot get response from grpc health endpoint for service {}",
-            service,
-        ));
+        .unwrap_or_else(|e| {
+            panic!(
+                "Cannot get response from grpc health endpoint for service {}: {}",
+                service, e
+            )
+        });
     let status = response.into_inner().status;
     if status != ServingStatus::Serving as i32 {
         panic!(
