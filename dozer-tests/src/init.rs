585e20d0ce688b3965bfd6765cb68689e2bf67fa
VG <vivekg342@gmail.com>
2023-01-23 14:49:16 +0800

    chore: include a panic hook

--- dozer-tests/src/init.rs
+++ dozer-tests/src/init.rs
@@ -1,5 +1,8 @@
 use std::{path::Path, process::Command, sync::Once};
 
+use dozer_core::dag::errors::ExecutionError;
+use dozer_types::log::{debug, error};
+
 fn download(folder_name: &str) {
     let path = std::env::current_dir()
         .unwrap()
@@ -22,5 +25,30 @@ pub fn init() {
     INIT.call_once(|| {
         dozer_tracing::init_telemetry(false).unwrap();
         download("actor");
+
+        set_panic_hook();
     });
+}
+
+fn set_panic_hook() {
+    std::panic::set_hook(Box::new(move |panic_info| {
+        if let Some(e) = panic_info
+            .payload()
+            .downcast_ref::<dozer_core::dag::errors::ExecutionError>()
+        {
+            error!("{}", e);
+            debug!("{:?}", e);
+        // All the pipeline errors are captured here
+        } else if let Some(e) = panic_info.payload().downcast_ref::<ExecutionError>() {
+            error!("{}", e);
+            debug!("{:?}", e);
+        // If any errors are sent as strings.
+        } else if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+            error!("{s:?}");
+        } else {
+            error!("{}", panic_info);
+        }
+
+        std::process::exit(1);
+    }));
 }
