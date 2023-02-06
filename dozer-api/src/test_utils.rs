fc37d6ae0cf0dcf8af6532eb94acb2cf02ff804d
VG <vivekg342@gmail.com>
2023-02-06 18:04:58 +0800

    chore: fix e2e test cases

--- dozer-api/src/test_utils.rs
+++ dozer-api/src/test_utils.rs
@@ -63,8 +63,6 @@ pub fn get_endpoint() -> ApiEndpoint {
     ApiEndpoint {
         name: "films".to_string(),
         path: "/films".to_string(),
-        // sql: Some("select film_id, description, rental_rate, release_year, updated_at from film where 1=1;"
-        //     .to_string()),
         index: Some(ApiIndex {
             primary_key: vec!["film_id".to_string()],
         }),
