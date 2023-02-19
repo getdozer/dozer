a06e04ddc1512a5aa9291babe1ea30b33fd5a749
xudong963 <wxd963996380@gmail.com>
2023-02-19 13:48:48 +0800

    rich test

--- dozer-tests/src/tests/sql/python_udf.rs
+++ dozer-tests/src/tests/sql/python_udf.rs
@@ -11,20 +11,26 @@ fn py_udf_query() {
       "#,
     ];
 
-    let record = Record {
+    let record1 = Record {
         schema_id: None,
         values: vec![Float(OrderedFloat(2.0)), Float(OrderedFloat(3.0))],
         version: None,
     };
-    let expected_results = vec![record];
+    let record2 = Record {
+        schema_id: None,
+        values: vec![Float(OrderedFloat(3.0)), Float(OrderedFloat(5.0))],
+        version: None,
+    };
+
+    let expected_results = vec![record1, record2];
 
     helper::compare_with_sqlite(
         &[],
         &queries,
         Some(&expected_results),
-        TestInstruction::List(vec![(
-            "t1",
-            "INSERT INTO t1(a, b) VALUES (1, 2)".to_string(),
-        )]),
+        TestInstruction::List(vec![
+            ("t1", "INSERT INTO t1(a, b) VALUES (1, 2)".to_string()),
+            ("t1", "INSERT INTO t1(a, b) VALUES (2, 3)".to_string()),
+        ]),
     );
 }
