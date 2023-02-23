13ed9bb0a805c0ed894fbfdfd622bb204fe1ab6a
Chloe Kim <chloe@getdozer.io>
2023-02-24 07:55:47 +0800

    feat: `decimal` and `binary` support for grpc (#1015)

--- dozer-api/src/generator/protoc/generator/implementation.rs
+++ dozer-api/src/generator/protoc/generator/implementation.rs
@@ -345,13 +345,11 @@ fn convert_dozer_type_to_proto_type(field_type: FieldType) -> Result<String, Gen
         FieldType::Boolean => Ok("bool".to_owned()),
         FieldType::String => Ok("string".to_owned()),
         FieldType::Text => Ok("string".to_owned()),
-        FieldType::Decimal => Ok("double".to_owned()),
+        FieldType::Binary => Ok("bytes".to_owned()),
+        FieldType::Decimal => Ok("dozer.types.RustDecimal".to_owned()),
         FieldType::Timestamp => Ok("google.protobuf.Timestamp".to_owned()),
         FieldType::Date => Ok("string".to_owned()),
         FieldType::Bson => Ok("google.protobuf.Any".to_owned()),
         FieldType::Point => Ok("dozer.types.PointType".to_owned()),
-        _ => Err(GenerationError::DozerToProtoTypeNotSupported(format!(
-            "{field_type:?}"
-        ))),
     }
 }
