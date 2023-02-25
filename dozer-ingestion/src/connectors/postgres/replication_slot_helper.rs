ab92f8b058b7bf63ee214bc9faf8d84f1abe3c2e
hoangnh93 <hoangnguyenhong1993@gmail.com>
2023-02-25 17:32:00 +0700

    fix: add tests for create and drop replication slot

--- dozer-ingestion/src/connectors/postgres/replication_slot_helper.rs
+++ dozer-ingestion/src/connectors/postgres/replication_slot_helper.rs
@@ -68,3 +68,198 @@ impl ReplicationSlotHelper {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use std::ops::Deref;
+    use std::sync::Arc;
+
+    use dozer_types::models::app_config::Config;
+    use dozer_types::models::connection::ConnectionConfig;
+    use postgres::NoTls;
+    use serial_test::serial;
+    use tokio_postgres::config::ReplicationMode;
+
+    use crate::errors::{ConnectorError, PostgresConnectorError};
+    use crate::test_util::run_connector_test;
+
+    use super::ReplicationSlotHelper;
+
+    fn get_config(app_config: Config) -> tokio_postgres::Config {
+        if let Some(ConnectionConfig::Postgres(connection)) =
+            &app_config.connections.get(0).unwrap().config
+        {
+            let mut config = tokio_postgres::Config::new();
+            config
+                .dbname(&connection.database)
+                .user(&connection.user)
+                .host(&connection.host)
+                .port(connection.port as u16)
+                .replication_mode(ReplicationMode::Logical)
+                .deref()
+                .clone()
+        } else {
+            panic!("Postgres config was expected")
+        }
+    }
+
+    #[test]
+    #[serial]
+    fn test_connector_replication_slot_create_successfully() {
+        run_connector_test("postgres", |app_config| {
+            let config = get_config(app_config);
+
+            let client = postgres::Config::from(config.clone())
+                .connect(NoTls)
+                .unwrap();
+
+            let client_ref = Arc::new(RefCell::new(client));
+
+            client_ref
+                .borrow_mut()
+                .simple_query("BEGIN READ ONLY ISOLATION LEVEL REPEATABLE READ;")
+                .unwrap();
+
+            let actual = ReplicationSlotHelper::create_replication_slot(client_ref.clone(), "test");
+
+            assert!(actual.is_ok());
+
+            match actual {
+                Err(_) => panic!("Validation should fail"),
+                Ok(result) => {
+                    if let Some(address) = result {
+                        assert_ne!(address, "")
+                    } else {
+                        panic!("Validation should fail")
+                    }
+                }
+            }
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn test_connector_replication_slot_create_failed_if_existed() {
+        run_connector_test("postgres", |app_config| {
+            let slot_name = "test";
+            let config = get_config(app_config);
+
+            let client = postgres::Config::from(config.clone())
+                .connect(NoTls)
+                .unwrap();
+
+            let client_ref = Arc::new(RefCell::new(client));
+
+            client_ref
+                .borrow_mut()
+                .simple_query("BEGIN READ ONLY ISOLATION LEVEL REPEATABLE READ;")
+                .unwrap();
+
+            let create_replication_slot_query = format!(
+                r#"CREATE_REPLICATION_SLOT {:?} LOGICAL "pgoutput" USE_SNAPSHOT"#,
+                slot_name
+            );
+
+            client_ref
+                .borrow_mut()
+                .simple_query(&create_replication_slot_query)
+                .expect("failed");
+
+            let actual =
+                ReplicationSlotHelper::create_replication_slot(client_ref.clone(), slot_name);
+
+            assert!(actual.is_err());
+
+            match actual {
+                Ok(_) => panic!("Validation should fail"),
+                Err(e) => {
+                    assert!(matches!(e, ConnectorError::PostgresConnectorError(_)));
+
+                    if let ConnectorError::PostgresConnectorError(cnn_err) = e {
+                        if let PostgresConnectorError::CreateSlotError(_, err) = cnn_err {
+                            assert_eq!(
+                                err.as_db_error().unwrap().message(),
+                                format!("replication slot \"{slot_name}\" already exists")
+                            );
+                        } else {
+                            panic!("Unexpected error occurred");
+                        }
+                    } else {
+                        panic!("Unexpected error occurred");
+                    }
+                }
+            }
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn test_connector_replication_slot_drop_successfully() {
+        run_connector_test("postgres", |app_config| {
+            let slot_name = "test";
+            let config = get_config(app_config);
+
+            let client = postgres::Config::from(config.clone())
+                .connect(NoTls)
+                .unwrap();
+
+            let client_ref = Arc::new(RefCell::new(client));
+
+            client_ref
+                .borrow_mut()
+                .simple_query("BEGIN READ ONLY ISOLATION LEVEL REPEATABLE READ;")
+                .unwrap();
+
+            let create_replication_slot_query = format!(
+                r#"CREATE_REPLICATION_SLOT {:?} LOGICAL "pgoutput" USE_SNAPSHOT"#,
+                slot_name
+            );
+
+            client_ref
+                .borrow_mut()
+                .simple_query(&create_replication_slot_query)
+                .expect("failed");
+
+            let actual =
+                ReplicationSlotHelper::drop_replication_slot(client_ref.clone(), slot_name);
+
+            assert!(actual.is_ok());
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn test_connector_replication_slot_drop_failed_if_slot_not_exist() {
+        run_connector_test("postgres", |app_config| {
+            let slot_name = "test";
+            let config = get_config(app_config);
+
+            let client = postgres::Config::from(config.clone())
+                .connect(NoTls)
+                .unwrap();
+
+            let client_ref = Arc::new(RefCell::new(client));
+
+            client_ref
+                .borrow_mut()
+                .simple_query("BEGIN READ ONLY ISOLATION LEVEL REPEATABLE READ;")
+                .unwrap();
+
+            let actual =
+                ReplicationSlotHelper::drop_replication_slot(client_ref.clone(), slot_name);
+
+            assert!(actual.is_err());
+
+            match actual {
+                Ok(_) => panic!("Validation should fail"),
+                Err(e) => {
+                    assert_eq!(
+                        e.as_db_error().unwrap().message(),
+                        format!("replication slot \"{slot_name}\" does not exist")
+                    );
+                }
+            }
+        });
+    }
+}
