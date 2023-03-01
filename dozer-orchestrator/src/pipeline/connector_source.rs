732700eca27e5994e03d8a6e632c2770455898e9
Bei Chu <914745487@qq.com>
2023-03-01 11:19:04 +0800

    fix: Use same progress bar style (expect for color) for source and sink (#1100)

--- dozer-orchestrator/src/pipeline/connector_source.rs
+++ dozer-orchestrator/src/pipeline/connector_source.rs
@@ -22,7 +22,7 @@ fn attach_progress(multi_pb: Option<MultiProgress>) -> ProgressBar {
     let pb = ProgressBar::new_spinner();
     multi_pb.as_ref().map(|m| m.add(pb.clone()));
     pb.set_style(
-        ProgressStyle::with_template("{msg}: {spinner:.red} {pos} : {per_sec}")
+        ProgressStyle::with_template("{spinner:.red} {msg}: {pos}: {per_sec}")
             .unwrap()
             // For more spinners check out the cli-spinners project:
             // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
