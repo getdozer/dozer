f46984043cf904df3b2397d60b611c502cebb3c7
Chloe Kim <chloeminkyung@gmail.com>
2023-03-06 19:59:40 +0800

    refactor: average aggregator

--- dozer-sql/src/pipeline/aggregation/count.rs
+++ dozer-sql/src/pipeline/aggregation/count.rs
@@ -1,4 +1,6 @@
 use dozer_core::errors::ExecutionError::InvalidOperation;
+use dozer_types::ordered_float::OrderedFloat;
+use dozer_types::rust_decimal::Decimal;
 use crate::pipeline::aggregation::aggregator::Aggregator;
 use crate::pipeline::errors::PipelineError;
 use dozer_types::types::{Field, FieldType};
@@ -32,7 +34,7 @@ impl Aggregator for CountAggregator {
             Some(field_type) => {
                 if field_type == return_type {
                     self.current_state -= 1;
-                    Ok(Field::UInt(self.current_state))
+                    get_count(self.current_state, return_type)
                 }
                 else {
                     Err(PipelineError::InternalExecutionError(InvalidOperation(format!("Failed to delete due to mismatch field type {} with record: {} for {}", field_type, old, Count.to_string()))))
@@ -47,7 +49,7 @@ impl Aggregator for CountAggregator {
             Some(field_type) => {
                 if field_type == return_type {
                     self.current_state += 1;
-                    Ok(Field::UInt(self.current_state))
+                    get_count(self.current_state, return_type)
                 }
                 else {
                     Err(PipelineError::InternalExecutionError(InvalidOperation(format!("Failed to delete due to mismatch field type {} with record: {} for {}", field_type, new, Count.to_string()))))
@@ -55,5 +57,23 @@ impl Aggregator for CountAggregator {
             },
             None => Err(PipelineError::InternalExecutionError(InvalidOperation(format!("Failed to insert record: {} for {}", new, Count.to_string())))),
         }
+    }
+}
+
+fn get_count(count: u64, return_type: FieldType) -> Result<Field, PipelineError> {
+    match return_type {
+        FieldType::UInt => {
+            Ok(Field::UInt(count))
+        }
+        FieldType::Int => {
+            Ok(Field::Int(count as i64))
+        }
+        FieldType::Float => {
+            Ok(Field::Float(OrderedFloat::from(count as f64)))
+        }
+        FieldType::Decimal => {
+            Ok(Field::Decimal(Decimal::from(count as f64)))
+        }
+        _ => Err(PipelineError::InternalExecutionError(InvalidOperation(format!("Not supported return type {} for {}", return_type, Count.to_string())))),
     }
 }
