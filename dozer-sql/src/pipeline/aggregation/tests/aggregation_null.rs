e6f5703b6d7a1f0b1fd7b5fa8b7ec9c2782ae783
Snork <matteo.pelati@gmail.com>
2023-03-07 16:37:10 +0800

    enabled tests

--- dozer-sql/src/pipeline/aggregation/tests/aggregation_null.rs
+++ dozer-sql/src/pipeline/aggregation/tests/aggregation_null.rs
@@ -8,44 +8,45 @@ use crate::pipeline::aggregation::tests::aggregation_tests_utils::{
 use dozer_core::DEFAULT_PORT_HANDLE;
 
 use dozer_types::types::FieldType::Int;
+use dozer_types::types::{Field, Operation, Record};
 use std::collections::HashMap;
-//
-// #[test]
-// fn test_sum_aggregation_null() {
-//     let schema = init_input_schema(Int, "SUM");
-//     let (processor, tx) = init_processor(
-//         "SELECT Country, SUM(Salary) \
-//         FROM Users \
-//         WHERE Salary >= 1 GROUP BY Country",
-//         HashMap::from([(DEFAULT_PORT_HANDLE, schema)]),
-//     )
-//     .unwrap();
-//
-//     // Insert 100 for segment Italy
-//     /*
-//         NULL, 100.0
-//         -------------
-//         SUM = 100.0
-//     */
-//     let inp = Operation::Insert {
-//         new: Record::new(
-//             None,
-//             vec![
-//                 Field::Int(0),
-//                 Field::Null,
-//                 FIELD_100_INT.clone(),
-//                 FIELD_100_INT.clone(),
-//             ],
-//             None,
-//         ),
-//     };
-//     let out = output!(processor, inp, tx);
-//     let exp = vec![Operation::Insert {
-//         new: Record::new(None, vec![Field::Null, FIELD_100_INT.clone()], None),
-//     }];
-//     assert_eq!(out, exp);
-// }
-//
+
+#[test]
+fn test_sum_aggregation_null() {
+    let schema = init_input_schema(Int, "SUM");
+    let mut processor = init_processor(
+        "SELECT Country, SUM(Salary) \
+        FROM Users \
+        WHERE Salary >= 1 GROUP BY Country",
+        HashMap::from([(DEFAULT_PORT_HANDLE, schema)]),
+    )
+    .unwrap();
+
+    // Insert 100 for segment Italy
+    /*
+        NULL, 100.0
+        -------------
+        SUM = 100.0
+    */
+    let inp = Operation::Insert {
+        new: Record::new(
+            None,
+            vec![
+                Field::Int(0),
+                Field::Null,
+                FIELD_100_INT.clone(),
+                FIELD_100_INT.clone(),
+            ],
+            None,
+        ),
+    };
+    let out = output!(processor, inp);
+    let exp = vec![Operation::Insert {
+        new: Record::new(None, vec![Field::Null, FIELD_100_INT.clone()], None),
+    }];
+    assert_eq!(out, exp);
+}
+
 #[test]
 fn test_sum_aggregation_del_and_insert() {
     let schema = init_input_schema(Int, "COUNT");
