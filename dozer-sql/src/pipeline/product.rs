814171cf9b6889fd0d57a16b7bcaf1be8548c0ff
Dario Pizzamiglio <mediuminvader@protonmail.com>
2023-03-09 09:19:30 +0800

    Introduce Table processor

--- dozer-sql/src/pipeline/product.rs
+++ dozer-sql/src/pipeline/product.rs
@@ -4,4 +4,5 @@ mod processor;
 pub mod set;
 pub mod set_factory;
 mod set_processor;
+pub mod table;
 mod tests;
---
+++ dozer-sql/src/pipeline/product/table/factory.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+use dozer_core::{
+    errors::ExecutionError,
+    node::{OutputPortDef, OutputPortType, PortHandle, Processor, ProcessorFactory},
+    storage::lmdb_storage::LmdbExclusiveTransaction,
+    DEFAULT_PORT_HANDLE,
+};
+use dozer_types::types::Schema;
+
+use crate::pipeline::builder::IndexedTableWithJoins;
+use crate::pipeline::expression::builder::NameOrAlias;
+use crate::pipeline::{builder::SchemaSQLContext, expression::builder::extend_schema_source_def};
+
+use super::processor::TableProcessor;
+
+#[derive(Debug)]
+pub struct RelationProcessorFactory {
+    input_tables: IndexedTableWithJoins,
+}
+
+impl RelationProcessorFactory {
+    pub fn new(input_tables: IndexedTableWithJoins) -> Self {
+        Self { input_tables }
+    }
+}
+
+impl ProcessorFactory<SchemaSQLContext> for RelationProcessorFactory {
+    fn get_input_ports(&self) -> Vec<PortHandle> {
+        vec![DEFAULT_PORT_HANDLE]
+    }
+
+    fn get_output_ports(&self) -> Vec<OutputPortDef> {
+        vec![OutputPortDef::new(
+            DEFAULT_PORT_HANDLE,
+            OutputPortType::Stateless,
+        )]
+    }
+
+    fn get_output_schema(
+        &self,
+        _output_port: &PortHandle,
+        input_schemas: &HashMap<PortHandle, (Schema, SchemaSQLContext)>,
+    ) -> Result<(Schema, SchemaSQLContext), ExecutionError> {
+        if let Some((input_schema, query_context)) = input_schemas.get(&DEFAULT_PORT_HANDLE) {
+            let input_names = get_input_names(&self.input_tables);
+            if input_names.len() != 1 {
+                return Err(ExecutionError::InternalError(
+                    "Invalid Input".to_string().into(),
+                ));
+            }
+            let table = input_names[0].clone();
+
+            let extended_input_schema = extend_schema_source_def(input_schema, &table);
+            Ok((extended_input_schema, query_context.clone()))
+        } else {
+            Err(ExecutionError::InvalidPortHandle(DEFAULT_PORT_HANDLE))
+        }
+    }
+
+    fn build(
+        &self,
+        _input_schemas: HashMap<PortHandle, dozer_types::types::Schema>,
+        _output_schemas: HashMap<PortHandle, dozer_types::types::Schema>,
+        _txn: &mut LmdbExclusiveTransaction,
+    ) -> Result<Box<dyn Processor>, ExecutionError> {
+        Ok(Box::new(TableProcessor::new()))
+    }
+}
+
+pub fn get_input_names(input_tables: &IndexedTableWithJoins) -> Vec<NameOrAlias> {
+    let mut input_names = vec![];
+    input_names.push(input_tables.relation.0.clone());
+
+    for join in &input_tables.joins {
+        input_names.push(join.0.clone());
+    }
+    input_names
+}
