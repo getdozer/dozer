9701db2a935cc8ba63c8c7beb6abe659a48fd4bf
Chloe Kim <chloeminkyung@gmail.com>
2023-03-20 11:55:48 +0800

    refactor: operations test with proptest (failing test cases are commented out with error detail)
    Signed-off-by: Chloe Kim <chloeminkyung@gmail.com>

--- dozer-sql/src/pipeline/expression/scalar/tests.rs
+++ dozer-sql/src/pipeline/expression/scalar/tests.rs
@@ -3,10 +3,10 @@ mod cast;
 #[cfg(test)]
 mod datetime;
 #[cfg(test)]
+mod mathematical;
+#[cfg(test)]
 mod number;
 #[cfg(test)]
 mod scalar_common;
 #[cfg(test)]
 mod string;
-#[cfg(test)]
-mod mathematical;
