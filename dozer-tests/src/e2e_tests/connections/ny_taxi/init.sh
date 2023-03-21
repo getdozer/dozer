7853f73966df19d15adffe53195084225eac2850
Dario Pizzamiglio <mediuminvader@protonmail.com>
2023-03-21 18:26:03 +0800

    Add yello taxi dataset

--- dozer-tests/src/e2e_tests/connections/ny_taxi/init.sh
+++ dozer-tests/src/e2e_tests/connections/ny_taxi/init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 curl --create-dirs -o data/trips/fhvhv_tripdata_2022-01.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_2022-01.parquet
-curl --create-dirs -o data/zones/taxi_zone_lookup.csv https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
 chmod -R a+rx data
