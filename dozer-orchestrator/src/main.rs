27b853369fc681e155bbd8c1f576d19162725197
VG <vivekg342@gmail.com>
2023-03-22 13:28:34 +0800

    chore: handle errors properly (#1279)
    * chore: handle errors properly
    
    * chore: fix clippy
    
    * chore: remove configure
    
    * chore: remove query
    
    * chore: remove query
    
    * chore: fix fmt and clippy

--- dozer-orchestrator/src/main.rs
+++ dozer-orchestrator/src/main.rs
@@ -1,17 +1,19 @@
 use clap::Parser;
 use dozer_orchestrator::cli::generate_config_repl;
 use dozer_orchestrator::cli::types::{ApiCommands, AppCommands, Cli, Commands, ConnectorCommands};
-use dozer_orchestrator::cli::{configure, init_dozer, list_sources, LOGO};
+use dozer_orchestrator::cli::{init_dozer, list_sources, LOGO};
 use dozer_orchestrator::errors::{CliError, OrchestrationError};
 use dozer_orchestrator::simple::SimpleOrchestrator;
 use dozer_orchestrator::{set_ctrl_handler, set_panic_hook, Orchestrator};
-use dozer_types::log::{error, info};
+use dozer_types::tracing::{error, info};
 
-use std::process;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::{process, thread};
 
 fn main() {
+    set_panic_hook();
+
     if let Err(e) = run() {
         error!("{}", e);
         process::exit(1);
@@ -27,29 +29,11 @@ fn render_logo() {
 }
 
 fn run() -> Result<(), OrchestrationError> {
-    set_panic_hook();
-
     // Reloading trace layer seems impossible, so we are running Cli::parse in a closure
     // and then initializing it after reading the configuration. This is a hacky workaround, but it works.
 
-    let res = dozer_tracing::init_telemetry_closure(
-        None,
-        None,
-        || -> Result<(Cli, SimpleOrchestrator), CliError> {
-            let cli = Cli::parse();
-            let dozer = init_dozer(cli.config_path.clone())?;
-
-            Ok((cli, dozer))
-        },
-    );
-
-    let (cli, mut dozer) = match res {
-        Ok((cli, dozer)) => (cli, dozer),
-        Err(e) => {
-            error!("{}", e);
-            process::exit(1);
-        }
-    };
+    let cli = parse_and_generate()?;
+    let mut dozer = init_orchestrator(&cli)?;
 
     let running = Arc::new(AtomicBool::new(true));
     set_ctrl_handler(running.clone());
@@ -84,8 +68,14 @@ fn run() -> Result<(), OrchestrationError> {
             Commands::Api(api) => match api.command {
                 ApiCommands::Run => {
                     render_logo();
-
-                    dozer.run_api(running)
+                    let running_api = running.clone();
+                    let _api_thread = thread::spawn(move || {
+                        if let Err(e) = dozer.run_api(running_api) {
+                            std::panic::panic_any(e);
+                        }
+                    });
+                    while running.load(Ordering::SeqCst) {}
+                    Ok(())
                 }
                 ApiCommands::GenerateToken => {
                     let token = dozer.generate_token()?;
@@ -109,8 +99,9 @@ fn run() -> Result<(), OrchestrationError> {
                 dozer.migrate(force)
             }
             Commands::Clean => dozer.clean(),
-            Commands::Configure => configure(cli.config_path, running),
-            Commands::Init => generate_config_repl(),
+            Commands::Init => {
+                panic!("This should not happen as it is handled in parse_and_generate");
+            }
         }
     } else {
         render_logo();
@@ -118,4 +109,38 @@ fn run() -> Result<(), OrchestrationError> {
         let mut dozer = init_dozer(cli.config_path)?;
         dozer.run_all(running)
     }
+}
+
+// Some commands dont need to initialize the orchestrator
+// This function is used to run those commands
+fn parse_and_generate() -> Result<Cli, OrchestrationError> {
+    dozer_tracing::init_telemetry_closure(None, None, || -> Result<Cli, OrchestrationError> {
+        let cli = Cli::parse();
+
+        if let Some(Commands::Init) = cli.cmd {
+            if let Err(e) = generate_config_repl() {
+                error!("{}", e);
+                Err(e)
+            } else {
+                // We need to exit here, otherwise the orchestrator will be initialized
+                process::exit(0);
+            }
+        } else {
+            Ok(cli)
+        }
+    })
+}
+
+fn init_orchestrator(cli: &Cli) -> Result<SimpleOrchestrator, CliError> {
+    dozer_tracing::init_telemetry_closure(None, None, || -> Result<SimpleOrchestrator, CliError> {
+        let res = init_dozer(cli.config_path.clone());
+
+        match res {
+            Ok(dozer) => Ok(dozer),
+            Err(e) => {
+                error!("{}", e);
+                Err(e)
+            }
+        }
+    })
 }
