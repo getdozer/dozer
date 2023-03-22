9f0d91714e7870676f30d2200c698434179957ab
Karolis Gudiškis <karolisgud@gmail.com>
2023-03-22 18:26:38 +0200

    feature: Handle grpc API errors gracefully
    Closes #1224

--- dozer-api/src/grpc/typed/tests/utils.rs
+++ dozer-api/src/grpc/typed/tests/utils.rs
@@ -16,7 +16,8 @@ fn test_records_to_typed_response() {
     let service_desc = ProtoGenerator::read_schema(&path, "films").unwrap();
 
     let records = get_sample_records(schema);
-    let res = query_response_to_typed_response(records, service_desc.query.response_desc.clone());
+    let res = query_response_to_typed_response(records, service_desc.query.response_desc.clone())
+        .unwrap();
     let records = res
         .message
         .get_field_by_name(service_desc.query.response_desc.records_field.name());
