6d0fca054569441f87739de0a042798493e6a8cc
VG <vivekg342@gmail.com>
2023-03-30 21:58:32 +0800

    chore: fix cache bench

--- dozer-cache/benches/cache.rs
+++ dozer-cache/benches/cache.rs
@@ -1,36 +1,26 @@
+use std::path::Path;
+
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use dozer_cache::cache::expression::{self, FilterExpression, QueryExpression, Skip};
-use dozer_cache::cache::{index, test_utils, LmdbRwCacheManager, RwCache, RwCacheManager};
+use dozer_cache::cache::{
+    test_utils, CacheManagerOptions, LmdbRwCacheManager, RwCache, RwCacheManager,
+};
 use dozer_types::models::api_endpoint::ConflictResolution;
 use dozer_types::parking_lot::Mutex;
 use dozer_types::serde_json::Value;
 use dozer_types::types::{Field, Record, Schema};
 
-fn insert(cache: &Mutex<Box<dyn RwCache>>, schema: &Schema, n: usize) {
+fn insert(cache: &Mutex<Box<dyn RwCache>>, schema: &Schema, n: usize, commit_size: usize) {
     let mut cache = cache.lock();
 
     let val = format!("bar_{n}");
-
     let mut record = Record::new(schema.identifier, vec![Field::String(val.clone())], None);
 
     cache.insert(&mut record).unwrap();
-    let key = index::get_primary_key(&[0], &[Field::String(val)]);
 
-    let _get_record = cache.get(&key).unwrap();
-}
-
-fn delete(cache: &Mutex<Box<dyn RwCache>>, n: usize) {
-    let mut cache = cache.lock();
-    let val = format!("bar_{n}");
-    let key = index::get_primary_key(&[0], &[Field::String(val)]);
-    let _ = cache.delete(&key);
-}
-
-fn get(cache: &Mutex<Box<dyn RwCache>>, n: usize) {
-    let cache = cache.lock();
-    let val = format!("bar_{n}");
-    let key = index::get_primary_key(&[0], &[Field::String(val)]);
-    let _get_record = cache.get(&key).unwrap();
+    if n % commit_size == 0 {
+        cache.commit().unwrap();
+    }
 }
 
 fn query(cache: &Mutex<Box<dyn RwCache>>, _n: usize) {
@@ -51,7 +41,21 @@ fn query(cache: &Mutex<Box<dyn RwCache>>, _n: usize) {
 
 fn cache(c: &mut Criterion) {
     let (schema, secondary_indexes) = test_utils::schema_0();
-    let cache_manager = LmdbRwCacheManager::new(Default::default()).unwrap();
+
+    let path = std::env::var("CACHE_BENCH_PATH").unwrap_or(".dozer".to_string());
+    let commit_size = std::env::var("CACHE_BENCH_COMMIT_SIZE").unwrap_or("".to_string());
+    let commit_size: usize = commit_size.parse().unwrap_or(1000);
+
+    let max_size = std::env::var("CACHE_BENCH_COMMIT_SIZE").unwrap_or("".to_string());
+    let max_size: usize = max_size.parse().unwrap_or(49999872000);
+
+    let cache_manager = LmdbRwCacheManager::new(CacheManagerOptions {
+        max_db_size: 1000,
+        max_size,
+        path: Some(Path::new(&path).to_path_buf()),
+        ..Default::default()
+    })
+    .unwrap();
     let cache = Mutex::new(
         cache_manager
             .create_cache(
@@ -63,17 +67,11 @@ fn cache(c: &mut Criterion) {
     );
 
     let size: usize = 1000000;
-    c.bench_with_input(BenchmarkId::new("cache_insert", size), &size, |b, &s| {
-        b.iter_batched(
-            || delete(&cache, s),
-            |_| insert(&cache, &schema, s),
-            criterion::BatchSize::NumIterations(1),
-        )
-    });
-
-    c.bench_with_input(BenchmarkId::new("cache_get", size), &size, |b, &s| {
+    let mut idx = 0;
+    c.bench_with_input(BenchmarkId::new("cache_insert", size), &size, |b, &_s| {
         b.iter(|| {
-            get(&cache, s);
+            insert(&cache, &schema, idx, commit_size);
+            idx += 1;
         })
     });
 
