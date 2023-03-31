2184c3f82fc9a17d31d4b497405d4353816054bb
VG <vivekg342@gmail.com>
2023-03-31 15:32:44 +0800

    chore: fix duplicate sources (#1353)

--- dozer-orchestrator/src/pipeline/source_builder.rs
+++ dozer-orchestrator/src/pipeline/source_builder.rs
@@ -4,12 +4,13 @@ use dozer_core::appsource::{AppSource, AppSourceManager};
 use dozer_ingestion::connectors::TableInfo;
 use dozer_sql::pipeline::builder::SchemaSQLContext;
 use dozer_types::indicatif::MultiProgress;
+use dozer_types::models::connection::Connection;
 use dozer_types::models::source::Source;
 use std::collections::HashMap;
 use std::sync::Arc;
 
 pub struct SourceBuilder<'a> {
-    grouped_connections: HashMap<String, Vec<Source>>,
+    grouped_connections: HashMap<Connection, Vec<Source>>,
     progress: Option<&'a MultiProgress>,
 }
 
@@ -17,7 +18,7 @@ const SOURCE_PORTS_RANGE_START: u16 = 1000;
 
 impl<'a> SourceBuilder<'a> {
     pub fn new(
-        grouped_connections: HashMap<String, Vec<Source>>,
+        grouped_connections: HashMap<Connection, Vec<Source>>,
         progress: Option<&'a MultiProgress>,
     ) -> Self {
         Self {
@@ -32,7 +33,7 @@ impl<'a> SourceBuilder<'a> {
         let mut ports = HashMap::new();
         for (conn, sources_group) in &self.grouped_connections {
             for source in sources_group {
-                ports.insert((conn.as_str(), source.name.as_str()), port);
+                ports.insert((conn.name.as_str(), source.name.as_str()), port);
                 port += 1;
             }
         }
@@ -46,39 +47,35 @@ impl<'a> SourceBuilder<'a> {
 
         let mut port: u16 = SOURCE_PORTS_RANGE_START;
 
-        for (conn, sources_group) in self.grouped_connections.clone() {
-            let first_source = sources_group.get(0).unwrap();
+        for (connection, sources_group) in &self.grouped_connections {
+            let mut ports = HashMap::new();
+            let mut table_and_ports = vec![];
+            for source in sources_group {
+                ports.insert(source.name.clone(), port);
 
-            if let Some(connection) = &first_source.connection {
-                let mut ports = HashMap::new();
-                let mut table_and_ports = vec![];
-                for source in &sources_group {
-                    ports.insert(source.name.clone(), port);
+                table_and_ports.push((
+                    TableInfo {
+                        schema: source.schema.clone(),
+                        name: source.table_name.clone(),
+                        column_names: source.columns.clone(),
+                    },
+                    port,
+                ));
 
-                    table_and_ports.push((
-                        TableInfo {
-                            schema: source.schema.clone(),
-                            name: source.table_name.clone(),
-                            column_names: source.columns.clone(),
-                        },
-                        port,
-                    ));
-
-                    port += 1;
-                }
-
-                let source_factory = ConnectorSourceFactory::new(
-                    table_and_ports,
-                    connection.clone(),
-                    self.progress.cloned(),
-                )?;
-
-                asm.add(AppSource::new(
-                    conn.to_string(),
-                    Arc::new(source_factory),
-                    ports,
-                ))?;
+                port += 1;
             }
+
+            let source_factory = ConnectorSourceFactory::new(
+                table_and_ports,
+                connection.clone(),
+                self.progress.cloned(),
+            )?;
+
+            asm.add(AppSource::new(
+                connection.name.to_string(),
+                Arc::new(source_factory),
+                ports,
+            ))?;
         }
 
         Ok(asm)
