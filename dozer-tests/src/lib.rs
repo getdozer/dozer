02edb616779ad0d7eff224f8344635d0448e5c09
Dario Pizzamiglio <mediuminvader@protonmail.com>
2023-04-11 11:54:39 +0800

    set init and csv as tests

--- dozer-tests/src/lib.rs
+++ dozer-tests/src/lib.rs
@@ -2,13 +2,15 @@
 #[cfg(test)]
 mod cache_tests;
 
+#[cfg(feature = "mongodb")]
+#[cfg(test)]
+mod init;
+
+#[cfg(feature = "mongodb")]
+#[cfg(test)]
+mod read_csv;
+
 #[cfg(test)]
 mod tests;
 
 pub mod e2e_tests;
-
-#[cfg(feature = "mongodb")]
-mod init;
-
-#[cfg(feature = "mongodb")]
-mod read_csv;
