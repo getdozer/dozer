ec12a05a36f919c0e1ca38571050f7067fe7f6a3
Ainur Timerbaev <ainur.timerbaev@superawesome.com>
2023-04-21 13:13:36 +0100

    Linter

--- dozer-ingestion/src/connectors/snowflake/schema_helper.rs
+++ dozer-ingestion/src/connectors/snowflake/schema_helper.rs
@@ -58,7 +58,7 @@ impl SchemaHelper {
             "BOOLEAN" => Ok(FieldType::Boolean),
             "DATE" => Ok(FieldType::Date),
             "TIMESTAMP_LTZ" | "TIMESTAMP_NTZ" | "TIMESTAMP_TZ" => Ok(FieldType::Timestamp),
-// TODO: proper type handling for VARIANT and TIME
+            // TODO: proper type handling for VARIANT and TIME
             "VARIANT" => Ok(FieldType::String),
             "TIME" => Ok(FieldType::String),
             _ => Err(SnowflakeSchemaError::ColumnTypeNotSupported(format!(
