e2c4920328606b2cbbe26fb27e19f36112da0d06
chubei <914745487@qq.com>
2023-05-04 16:43:42 +0800

    fix: Respect system's native certificate store when connecting to postgres

--- dozer-ingestion/src/connectors/postgres/connection/helper.rs
+++ dozer-ingestion/src/connectors/postgres/connection/helper.rs
@@ -1,5 +1,5 @@
 use crate::errors::{ConnectorError, PostgresConnectorError};
-use dozer_types::log::error;
+use dozer_types::log::{debug, error};
 use dozer_types::models::connection::ConnectionConfig;
 use tokio_postgres::Client;
 
@@ -21,9 +21,17 @@ pub fn map_connection_config(
 }
 
 pub async fn connect(config: tokio_postgres::Config) -> Result<Client, PostgresConnectorError> {
+    let mut roots = rustls::RootCertStore::empty();
+    for cert in
+        rustls_native_certs::load_native_certs().map_err(PostgresConnectorError::LoadNativeCerts)?
+    {
+        if let Err(e) = roots.add(&rustls::Certificate(cert.0)) {
+            debug!("Failed to add certificate: {}", e);
+        }
+    }
     let rustls_config = rustls::ClientConfig::builder()
         .with_safe_defaults()
-        .with_root_certificates(rustls::RootCertStore::empty())
+        .with_root_certificates(roots)
         .with_no_client_auth();
     let (client, connection) = config
         .connect(tokio_postgres_rustls::MakeRustlsConnect::new(rustls_config))
