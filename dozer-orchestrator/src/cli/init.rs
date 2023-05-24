d6fd7f66c2cf9a3d38dd18933ecb469c05d2363f
Karolis Gudiškis <karolisgud@gmail.com>
2023-05-24 08:51:43 +0300

    feat: Create empty directories for lambdas and queries during init

--- dozer-orchestrator/src/cli/init.rs
+++ dozer-orchestrator/src/cli/init.rs
@@ -1,3 +1,4 @@
+use std::path::{Path, PathBuf};
 use crate::errors::{CliError, OrchestrationError};
 use dozer_types::models::app_config::{default_cache_dir, default_home_dir, get_cache_dir};
 use dozer_types::{
@@ -16,6 +17,8 @@ use rustyline::{
 };
 use rustyline::{error::ReadlineError, Editor};
 use rustyline_derive::{Helper, Highlighter, Hinter, Validator};
+use dozer_types::constants::{DEFAULT_LAMBDAS_DIRECTORY, DEFAULT_QUERIES_DIRECTORY};
+use dozer_types::log::warn;
 
 #[derive(Helper, Highlighter, Hinter, Validator)]
 pub struct InitHelper {}
@@ -188,6 +191,20 @@ pub fn generate_config_repl() -> Result<(), OrchestrationError> {
                     .map_err(OrchestrationError::FailedToWriteConfigYaml)?;
                 info!("\nGenerating configuration at: {}\n• More details about our config: https://getdozer.io/docs/reference/configuration/introduction\n• Connector & Sources: https://getdozer.io/docs/reference/configuration/connectors\n• Endpoints: https://getdozer.io/docs/reference/configuration/endpoints/",
                    yaml_path.to_owned());
+
+                let path = PathBuf::from(yaml_path);
+                if let Some(dir) = path.parent() {
+                    let queries_path = Path::new(dir).join(DEFAULT_QUERIES_DIRECTORY);
+                    if let Err(_e) = std::fs::create_dir(queries_path) {
+                        warn!("Cannot create queries directory");
+                    }
+
+                    let lambdas_path = Path::new(dir).join(DEFAULT_LAMBDAS_DIRECTORY);
+                    if let Err(_e) = std::fs::create_dir(lambdas_path) {
+                        warn!("Cannot create lambdas directory");
+                    }
+                }
+
                 Ok(())
             }),
         ),
