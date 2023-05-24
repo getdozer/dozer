72f2861d86551be1f3c6f3d24235bfadac5453bc
Karolis Gudiškis <karolisgud@gmail.com>
2023-05-24 09:57:25 +0300

    feat: Create empty directories for lambdas and queries during init (#1591)
    * feat: Create empty directories for lambdas and queries during init

--- dozer-sql/src/pipeline/product/tests/pipeline_test.rs
+++ dozer-sql/src/pipeline/product/tests/pipeline_test.rs
@@ -474,7 +474,7 @@ impl Sink for TestSink {
 #[test]
 #[ignore]
 fn test_pipeline_builder() {
-    let _ = dozer_tracing::init_telemetry(None, None);
+    dozer_tracing::init_telemetry(None, None);
 
     let mut pipeline = AppPipeline::new();
 
