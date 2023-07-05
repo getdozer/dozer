a491a33166f7a28aeb7d89b3d9c38244027fe26b
Chloe Kim <chloeminkyung@gmail.com>
2023-07-05 17:35:01 +0800

    fix: added unit sql test for wildcard

--- dozer-tests/src/tests/e2e/basic_sql_wildcard.rs
+++ dozer-tests/src/tests/e2e/basic_sql_wildcard.rs
@@ -1,54 +1,24 @@
-use dozer_types::grpc_types::{ingest::IngestRequest, types};
+use dozer_types::grpc_types::common::QueryRequest;
+use std::time::Duration;
 
 use super::DozerE2eTest;
 
 #[tokio::test]
-#[ignore = "Wildcard implementation may have a bug. This test fails about one out of 5 times"]
 async fn test_e2e_wildcard() {
     let mut test = DozerE2eTest::new(include_str!("./fixtures/basic_sql_wildcard.yaml")).await;
 
-    let ingest_client = test.ingest_service_client.as_mut().unwrap();
+    // Give dozer some time to process the records.
+    tokio::time::sleep(Duration::from_millis(1000)).await;
 
-    ingest_client
-        .ingest(IngestRequest {
-            schema_name: "table3".to_string(),
-            new: Some(types::Record {
-                values: vec![
-                    types::Value {
-                        value: Some(types::value::Value::IntValue(1)),
-                    },
-                    types::Value {
-                        value: Some(types::value::Value::IntValue(11)),
-                    },
-                ],
-                version: 1,
-            }),
-            seq_no: 1,
-            ..Default::default()
+    let common_client = &mut test.common_service_client;
+
+    let res = common_client
+        .query(QueryRequest {
+            endpoint: "wildcard_res".to_string(),
+            query: None,
         })
         .await
         .unwrap();
-
-    ingest_client
-        .ingest(IngestRequest {
-            schema_name: "table4".to_string(),
-            new: Some(types::Record {
-                values: vec![
-                    types::Value {
-                        value: Some(types::value::Value::IntValue(1)),
-                    },
-                    types::Value {
-                        value: Some(types::value::Value::IntValue(11)),
-                    },
-                ],
-                version: 1,
-            }),
-            seq_no: 2,
-            ..Default::default()
-        })
-        .await
-        .unwrap();
-
-    // Wait for api to process the records.
-    tokio::time::sleep(std::time::Duration::from_millis(2000)).await;
+    let res = res.into_inner();
+    assert_eq!(res.records.len(), 6);
 }
