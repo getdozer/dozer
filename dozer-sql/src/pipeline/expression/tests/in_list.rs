680236379194e3cdb2acb59e6cb055f9d3a2232b
hi-rustin <rustin.liu@gmail.com>
2023-07-07 15:47:56 +0800

    Better test code
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- dozer-sql/src/pipeline/expression/tests/in_list.rs
+++ dozer-sql/src/pipeline/expression/tests/in_list.rs
@@ -17,36 +17,27 @@ fn test_in_list() {
     );
     assert_eq!(f, Field::Boolean(true));
 
+    let schema = Schema::empty()
+        .field(
+            FieldDefinition::new(
+                String::from("age"),
+                FieldType::Int,
+                false,
+                SourceDefinition::Dynamic,
+            ),
+            false,
+        )
+        .clone();
     let f = run_fct(
         "SELECT age IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) FROM users",
-        Schema::empty()
-            .field(
-                FieldDefinition::new(
-                    String::from("age"),
-                    FieldType::Int,
-                    false,
-                    SourceDefinition::Dynamic,
-                ),
-                false,
-            )
-            .clone(),
+        schema.clone(),
         vec![Field::Int(42)],
     );
     assert_eq!(f, Field::Boolean(false));
 
     let f = run_fct(
         "SELECT age IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 42) FROM users",
-        Schema::empty()
-            .field(
-                FieldDefinition::new(
-                    String::from("age"),
-                    FieldType::Int,
-                    false,
-                    SourceDefinition::Dynamic,
-                ),
-                false,
-            )
-            .clone(),
+        schema.clone(),
         vec![Field::Int(42)],
     );
     assert_eq!(f, Field::Boolean(true));
@@ -68,36 +59,27 @@ fn test_not_in_list() {
     );
     assert_eq!(f, Field::Boolean(false));
 
+    let schema = Schema::empty()
+        .field(
+            FieldDefinition::new(
+                String::from("age"),
+                FieldType::Int,
+                false,
+                SourceDefinition::Dynamic,
+            ),
+            false,
+        )
+        .clone();
     let f = run_fct(
         "SELECT age NOT IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) FROM users",
-        Schema::empty()
-            .field(
-                FieldDefinition::new(
-                    String::from("age"),
-                    FieldType::Int,
-                    false,
-                    SourceDefinition::Dynamic,
-                ),
-                false,
-            )
-            .clone(),
+        schema.clone(),
         vec![Field::Int(42)],
     );
     assert_eq!(f, Field::Boolean(true));
 
     let f = run_fct(
         "SELECT age NOT IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 42) FROM users",
-        Schema::empty()
-            .field(
-                FieldDefinition::new(
-                    String::from("age"),
-                    FieldType::Int,
-                    false,
-                    SourceDefinition::Dynamic,
-                ),
-                false,
-            )
-            .clone(),
+        schema.clone(),
         vec![Field::Int(42)],
     );
     assert_eq!(f, Field::Boolean(false));
