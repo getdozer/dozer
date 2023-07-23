44b96061ee7251aee2e3b86920afa03ea5813bdb
VG <vivekg342@gmail.com>
2023-07-23 18:02:19 +0800

    feat: add processor record

---
+++ dozer-types/src/types/ref_types.rs
@@ -0,0 +1,46 @@
+use std::hash::Hash;
+use std::sync::Arc;
+
+use super::{Field, Lifetime, Record};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ProcessorRecord {
+    /// List of values, following the definitions of `fields` of the associated schema
+    pub values: Vec<RefOrField>,
+
+    /// Time To Live for this record. If the value is None, the record will never expire.
+    pub lifetime: Option<Lifetime>,
+
+    // Indexes of the fields to be used if this is a reference record
+    pub index: Vec<u32>,
+}
+
+impl ProcessorRecord {
+    pub fn new() -> Self {
+        ProcessorRecord {
+            values: Vec::new(),
+            lifetime: None,
+            index: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+
+pub enum RefOrField {
+    Ref(RecordRef),
+    Field(Field),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RecordRef(Arc<Record>);
+
+impl From<Record> for ProcessorRecord {
+    fn from(record: Record) -> Self {
+        let mut ref_record = ProcessorRecord::new();
+        for field in record.values {
+            ref_record.values.push(RefOrField::Field(field));
+        }
+        ref_record
+    }
+}
