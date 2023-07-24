79c5a2ee6bec866cdc56ac5e38717bad2c0bbae6
chubei <914745487@qq.com>
2023-07-24 11:42:35 +0800

    fix: Don't expose `ProcessorRecordRef` implementation detail

--- dozer-core/src/record_store.rs
+++ dozer-core/src/record_store.rs
@@ -61,12 +61,12 @@ impl RecordWriter for PrimaryKeyLookupRecordWriter {
     fn write(&mut self, op: ProcessorOperation) -> Result<ProcessorOperation, RecordWriterError> {
         match op {
             ProcessorOperation::Insert { new } => {
-                let new_key = new.0.get_key(&self.schema.primary_index);
+                let new_key = new.get_record().get_key(&self.schema.primary_index);
                 self.index.insert(new_key, new.clone());
                 Ok(ProcessorOperation::Insert { new })
             }
             ProcessorOperation::Delete { mut old } => {
-                let old_key = old.0.get_key(&self.schema.primary_index);
+                let old_key = old.get_record().get_key(&self.schema.primary_index);
                 old = self
                     .index
                     .remove_entry(&old_key)
@@ -75,13 +75,13 @@ impl RecordWriter for PrimaryKeyLookupRecordWriter {
                 Ok(ProcessorOperation::Delete { old })
             }
             ProcessorOperation::Update { mut old, new } => {
-                let old_key = old.0.get_key(&self.schema.primary_index);
+                let old_key = old.get_record().get_key(&self.schema.primary_index);
                 old = self
                     .index
                     .remove_entry(&old_key)
                     .ok_or(RecordWriterError::RecordNotFound)?
                     .1;
-                let new_key = new.0.get_key(&self.schema.primary_index);
+                let new_key = new.get_record().get_key(&self.schema.primary_index);
                 self.index.insert(new_key, new.clone());
                 Ok(ProcessorOperation::Update { old, new })
             }
