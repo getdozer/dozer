9549e2de34f088253b396d1a2b5a484c42fb9a35
chubei <914745487@qq.com>
2023-08-10 18:42:55 +0800

    fix: Should use tokio mutex in async context

--- dozer-log/src/storage/local.rs
+++ dozer-log/src/storage/local.rs
@@ -7,13 +7,15 @@ use camino::Utf8Path;
 use dozer_types::{
     bytes::Bytes,
     grpc_types::internal::{self, storage_response},
-    parking_lot::Mutex,
     tonic::async_trait,
     tracing::debug,
 };
 use futures_util::{stream::BoxStream, StreamExt};
 use tempdir::TempDir;
-use tokio::io::{AsyncWriteExt, BufReader};
+use tokio::{
+    io::{AsyncWriteExt, BufReader},
+    sync::Mutex,
+};
 use tokio_util::io::ReaderStream;
 
 use super::{Error, ListObjectsOutput, Object, Storage};
@@ -69,7 +71,7 @@ impl Storage for LocalStorage {
             .to_str()
             .ok_or(Error::NonUtf8Path(path.to_path_buf()))?
             .to_string();
-        let mut multipart_uploads = self.multipart_uploads.lock();
+        let mut multipart_uploads = self.multipart_uploads.lock().await;
         match multipart_uploads.entry(key) {
             Entry::Occupied(mut entry) => {
                 entry.get_mut().insert(upload_id.clone(), temp_dir);
@@ -90,6 +92,7 @@ impl Storage for LocalStorage {
     ) -> Result<String, Error> {
         self.multipart_uploads
             .lock()
+            .await
             .get(&key)
             .and_then(|uploads| uploads.get(&upload_id))
             .ok_or(Error::UploadNotFound {
@@ -125,6 +128,7 @@ impl Storage for LocalStorage {
 
         self.multipart_uploads
             .lock()
+            .await
             .get_mut(&key)
             .and_then(|uploads| uploads.remove(&upload_id))
             .ok_or(Error::UploadNotFound {
