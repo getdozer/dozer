1b55572f8098aea72fd278205c5dea702553f521
Chloe Kim <chloeminkyung@gmail.com>
2023-08-29 12:01:10 +0800

    feat: udfs config flowing into sql engine

--- dozer-sql/src/pipeline/expression/tests/onnx.rs
+++ dozer-sql/src/pipeline/expression/tests/onnx.rs
@@ -7,7 +7,7 @@ use crate::pipeline::expression::onnx_udf::evaluate_onnx_udf;
 #[test]
 fn test_standard() {
     let environment = Environment::builder()
-        .with_name("dozer_onnx")
+        .with_name("a1f19e3a24bd49219a0f0060200a258b")
         .with_log_level(LoggingLevel::Verbose)
         .build()
         .unwrap()
@@ -16,12 +16,11 @@ fn test_standard() {
     let session = SessionBuilder::new(&environment).unwrap()
         .with_optimization_level(GraphOptimizationLevel::Level1).unwrap()
         .with_intra_threads(1).unwrap()
-        .with_model_from_file(Path::new("/Users/chloeminkyung/CLionProjects/dozer/dozer-sql/src/pipeline/expression/tests/models/upsample.onnx"))
+        .with_model_from_file(Path::new("/Users/chloeminkyung/CLionProjects/dozer/dozer-sql/src/pipeline/expression/tests/models/vectorizer.onnx"))
         .expect("Could not read model from memory");
 
     let record = Record::new(vec![
-        Field::Int(1337),
-        Field::Float(OrderedFloat(10.10)),
+        Field::String("document".to_string()),
     ]);
 
     let res = evaluate_onnx_udf(&Schema::default(), &session, &vec![], &FieldType::String, &record);
