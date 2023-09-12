f8f34c18b50dfaa5029e7ce4519aedbdd2f2ef07
Karolis Gudiškis <karolisgud@gmail.com>
2023-09-12 13:20:22 +0300

    feat: Refactor process printer

--- dozer-cli/src/simple/cloud/deployer.rs
+++ dozer-cli/src/simple/cloud/deployer.rs
@@ -4,60 +4,80 @@ use crate::simple::token_layer::TokenLayer;
 use dozer_types::grpc_types::cloud::dozer_cloud_client::DozerCloudClient;
 
 use crate::errors::CloudError::GRPCCallError;
-use dozer_types::grpc_types::cloud::{
-    LogMessage, Secret, StartRequest, StartUpdate, StopRequest, StopResponse,
-};
-use dozer_types::log::{info, warn};
+use dozer_types::grpc_types::cloud::{Secret, StopRequest, StopResponse};
+use dozer_types::log::{error, info};
 
 use crate::progress_printer::ProgressPrinter;
+use dozer_types::grpc_types::cloud::AppResponse;
+use dozer_types::grpc_types::cloud::DeployAppRequest;
+use dozer_types::grpc_types::cloud::DeployAppResponse;
+use dozer_types::grpc_types::cloud::DeployStep;
+use dozer_types::grpc_types::cloud::File;
+use dozer_types::grpc_types::cloud::InfraRequest;
 
 pub async fn deploy_app(
     client: &mut DozerCloudClient<TokenLayer>,
-    app_id: &str,
+    app_id: &Option<String>,
     num_api_instances: i32,
-    steps: &mut ProgressPrinter,
     secrets: Vec<Secret>,
     allow_incompatible: bool,
+    files: Vec<File>,
 ) -> Result<(), CloudError> {
     let mut response = client
-        .start_dozer(StartRequest {
-            app_id: app_id.to_string(),
-            num_api_instances,
+        .deploy_application(DeployAppRequest {
+            app_id: app_id.clone(),
             secrets,
             allow_incompatible,
+            files,
+            infra: Some(InfraRequest { num_api_instances }),
         })
         .await?
         .into_inner();
 
-    while let Some(StartUpdate {
+    let mut printer = ProgressPrinter::new();
+    while let Some(DeployAppResponse {
         result,
-        current_step: _,
-        total_steps: _,
-        last_message,
+        completed: _,
+        error: error_message,
     }) = response.message().await.map_err(GRPCCallError)?
     {
-        if let Some(LogMessage { message, from }) = last_message {
-            for line in message.lines().collect::<Vec<_>>() {
-                info!("[{}] {line}", from);
-            }
+        if let Some(message) = error_message {
+            error!("{}", message);
         } else {
             match result {
-                Some(r) => {
-                    if r.success {
-                        steps.complete_step(Some(&format!(
-                            "Deployed {}\nEndpoint: {}",
-                            r.app_id, r.api_endpoint
-                        )));
+                Some(dozer_types::grpc_types::cloud::deploy_app_response::Result::AppCreated(
+                    AppResponse {
+                        app_id: new_app_id, ..
+                    },
+                )) => {
+                    if let Some(app_id) = app_id {
+                        info!("Updating {app_id} application");
                     } else {
-                        match r.error {
-                            Some(error) => warn!("Deployment failed. Error: {}", &error),
-                            None => warn!("Deployment failed"),
-                        }
+                        info!("Deploying new application {new_app_id}");
                     }
                 }
-                None => {
-                    steps.start_next_step();
+                Some(dozer_types::grpc_types::cloud::deploy_app_response::Result::Step(
+                    DeployStep {
+                        text,
+                        is_completed,
+                        current_step,
+                        ..
+                    },
+                )) => {
+                    if is_completed {
+                        printer.complete_step(current_step, &text);
+                    } else {
+                        printer.start_step(current_step, &text);
+                    }
                 }
+                Some(dozer_types::grpc_types::cloud::deploy_app_response::Result::LogMessage(
+                    message,
+                )) => {
+                    message.split('\n').for_each(|line| {
+                        info!("{}", line);
+                    });
+                }
+                None => {}
             }
         }
     }
