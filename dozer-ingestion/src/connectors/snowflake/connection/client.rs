4a5dd5b74d5aedee84d7119d30f4f5b77537b640
abcpro1 <abcpro11051@disroot.org>
2023-09-15 12:17:59 +0000

    fix: break out of an infinite loop

--- dozer-ingestion/src/connectors/snowflake/connection/client.rs
+++ dozer-ingestion/src/connectors/snowflake/connection/client.rs
@@ -519,6 +519,7 @@ fn exec_iter(pool: Pool, query: String) -> ExecIter {
                 }
             }
             conn.return_();
+            break;
         }
         Ok::<(), SnowflakeError>(())
     });
