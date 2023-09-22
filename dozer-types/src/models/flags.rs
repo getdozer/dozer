2b004018e1ade67d7930442df357727978e2fae7
VG <vivekg342@gmail.com>
2023-09-22 09:41:35 +0800

    chore: fix tests

--- dozer-types/src/models/flags.rs
+++ dozer-types/src/models/flags.rs
@@ -1,19 +1,16 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-#[derive(Debug, Serialize, JsonSchema, Deserialize, PartialEq, Eq, Clone, Default)]
+#[derive(Debug, Serialize, JsonSchema, Deserialize, PartialEq, Eq, Clone)]
 pub struct Flags {
     /// dynamic grpc enabled; Default: true
-
     #[serde(default = "default_true")]
     pub dynamic: bool,
     /// http1 + web support for grpc. This is required for browser clients.; Default: true
-
     #[serde(default = "default_true")]
     pub grpc_web: bool,
 
     /// push events enabled.; Default: true
-
-    #[serde(default = "default_push_events")]
+    #[serde(default = "default_true")]
     pub push_events: bool,
 
     /// require authentication to access grpc server reflection service if true.; Default: false
@@ -25,29 +22,41 @@ pub struct Flags {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub enable_probabilistic_optimizations: Option<EnableProbabilisticOptimizations>,
 }
+impl Default for Flags {
+    fn default() -> Self {
+        Self {
+            dynamic: true,
+            grpc_web: true,
+            push_events: true,
+            authenticate_server_reflection: false,
+            enable_probabilistic_optimizations: Default::default(),
+        }
+    }
+}
 
-#[derive(Debug, Serialize, JsonSchema, Default, Deserialize, PartialEq, Eq, Clone)]
+#[derive(Debug, Serialize, JsonSchema, Deserialize, PartialEq, Eq, Clone)]
 pub struct EnableProbabilisticOptimizations {
     /// enable probabilistic optimizations in set operations (UNION, EXCEPT, INTERSECT); Default: false
-
     #[serde(default = "default_false")]
     pub in_sets: bool,
 
     /// enable probabilistic optimizations in JOIN operations; Default: false
-
     #[serde(default = "default_false")]
     pub in_joins: bool,
 
     /// enable probabilistic optimizations in aggregations (SUM, COUNT, MIN, etc.); Default: false
-
     #[serde(default = "default_false")]
     pub in_aggregations: bool,
 }
-
-pub fn default_push_events() -> bool {
-    true
+impl Default for EnableProbabilisticOptimizations {
+    fn default() -> Self {
+        Self {
+            in_sets: false,
+            in_joins: false,
+            in_aggregations: false,
+        }
+    }
 }
-
 fn default_true() -> bool {
     true
 }
