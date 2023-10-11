d05e9c12549707163d29a68bdda1aba2812a0ab8
Jesse Bakker <jesse@getdozer.io>
2023-10-11 12:52:39 +0200

    Fix postgres field reordering during CDC

--- dozer-ingestion/tests/test_suite/data.rs
+++ dozer-ingestion/tests/test_suite/data.rs
@@ -45,3 +45,25 @@ pub fn cud_operations() -> (FieldsAndPk, Vec<Operation>) {
     ];
     (schema, operations)
 }
+
+pub fn reorder(
+    fields: &[FieldDefinition],
+    pk: &[usize],
+    operations: &[Operation],
+) -> (FieldsAndPk, Vec<Operation>) {
+    let reversed_fields = fields.iter().rev().cloned().collect();
+    let reversed_pk = pk.iter().map(|pk| fields.len() - 1 - pk).collect();
+
+    let mut reversed_operations = operations.to_vec();
+    for op in reversed_operations.iter_mut() {
+        match op {
+            Operation::Insert { new } => new.reverse(),
+            Operation::Update { old, new } => {
+                old.reverse();
+                new.reverse();
+            }
+            Operation::Delete { old } => old.reverse(),
+        }
+    }
+    ((reversed_fields, reversed_pk), reversed_operations)
+}
