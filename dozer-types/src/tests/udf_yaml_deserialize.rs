8591f5e06159583efe2ce4c5a6a1a45c900eaf2d
TachyonicBytes <support@tachyonicbytes.com>
2023-11-27 00:26:04 +0200

    Add wasm_udfs to the new config

--- dozer-types/src/tests/udf_yaml_deserialize.rs
+++ dozer-types/src/tests/udf_yaml_deserialize.rs
@@ -17,3 +17,22 @@ fn standard() {
     let expected = udf_conf;
     assert_eq!(expected, deserializer_result);
 }
+
+#[cfg(feature = "wasm")]
+#[test]
+fn standard_wasm() {
+    let udf_config = r#"
+    name: is_fraudulent
+    config: !Onnx
+      path: ./models/model_file
+  "#;
+    let deserializer_result = serde_yaml::from_str::<UdfConfig>(udf_config).unwrap();
+    let udf_conf = UdfConfig {
+        config: Some(UdfType::Wasm(WasmConfig {
+            path: "./models/model_file".to_string(),
+        })),
+        name: "is_fraudulent".to_string(),
+    };
+    let expected = udf_conf;
+    assert_eq!(expected, deserializer_result);
+}
