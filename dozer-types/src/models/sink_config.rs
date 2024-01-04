b51f475477924bcab3c35e8676fc139e33f9886d
Solomon <solomon@getdozer.io>
2024-01-04 09:14:53 +0100

    chore: make default options suitable for high throughput

--- dozer-types/src/models/sink_config.rs
+++ dozer-types/src/models/sink_config.rs
@@ -83,14 +83,14 @@ pub mod snowflake {
     }
 
     pub fn default_batch_size() -> usize {
-        100
+        1000000
     }
 
     pub fn default_batch_interval() -> Duration {
-        Duration::from_secs(5)
+        Duration::from_secs(1)
     }
 
     pub fn default_suspend_warehouse() -> bool {
-        false
+        true
     }
 }
