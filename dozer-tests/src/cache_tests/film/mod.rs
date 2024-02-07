488f2b6162c6b82244ec37360b7a43a4d2550022
Jesse <jesse@getdozer.io>
2024-02-07 14:20:42 +0100

    Fix CI (#2381)

--- dozer-tests/src/cache_tests/film/mod.rs
+++
@@ -1,7 +0,0 @@
-mod schema;
-
-pub use schema::{film_schema, Film};
-
-mod load_database;
-
-pub use load_database::load_database;
--- dozer-tests/src/lib.rs
+++ dozer-tests/src/lib.rs
@@ -1,16 +1,1 @@
-#[cfg(feature = "mongodb")]
-#[cfg(test)]
-mod cache_tests;
 
-#[cfg(feature = "mongodb")]
-#[cfg(test)]
-mod init;
-
-#[cfg(feature = "mongodb")]
-#[cfg(test)]
-mod read_csv;
-
-#[cfg(test)]
-mod tests;
-
-pub mod e2e_tests;
