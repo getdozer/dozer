4d28446cf0111526d848e78b560bce93bfc56cf8
Vaibhav Prabhu <prabhu.vaibhav02@gmail.com>
2024-02-09 19:16:31 +0530

    chore: delete dozer-api

--- dozer-api/src/grpc/common/mod.rs
+++
@@ -1,7 +0,0 @@
-mod service;
-pub use service::CommonService;
-
-#[cfg(test)]
-mod tests;
-
-pub const SERVICE_NAME: &str = "common";
--- dozer-api/src/grpc/health/mod.rs
+++
@@ -1,5 +0,0 @@
-mod service;
-pub use service::HealthService;
-
-#[cfg(test)]
-mod tests;
