c29a021c5c2682732845a800ae8f5b2209b64714
Karolis Gudiškis <karolisgud@gmail.com>
2024-02-26 13:11:23 +0200

    Fix precision

--- dozer-ingestion/oracle/src/connector/mapping.rs
+++ dozer-ingestion/oracle/src/connector/mapping.rs
@@ -55,7 +55,7 @@ fn map_data_type(
             "NVARCHAR2" => unimplemented!("convert NVARCHAR2 to String"),
             "INTEGER" => Ok(FieldType::I128),
             "NUMBER" => match (precision, scale) {
-                (Some(precision), Some(0)) if precision <= 22 => Ok(FieldType::Int),
+                (Some(precision), Some(0)) if precision <= 19 => Ok(FieldType::Int),
                 (_, Some(0)) => Ok(FieldType::I128),
                 _ => Ok(FieldType::Decimal),
             },
