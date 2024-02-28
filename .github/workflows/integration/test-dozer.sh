076e9dff490f13b5f4795d391bbfb1c1b5c49e4c
chubei <914745487@qq.com>
2024-02-28 16:03:45 +0800

    chore: Update integration test

--- .github/workflows/integration/test-dozer.sh
+++ .github/workflows/integration/test-dozer.sh
@@ -2,9 +2,3 @@ set -e
 
 # Check if dozer version matches `DOZER_VERSION`
 dozer -V | grep "$DOZER_VERSION"
-
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Run grpc ingest test because it doesn't need docker or ETH secrets.
-CARGO_TARGET_DIR=../ DOZER_BIN=dozer RUST_LOG=info "$HOME/.cargo/bin/cargo" run -p dozer-tests --bin dozer-tests -- grpc_ingest
