5a84688697d50ee38418c5a904c75d6540524714
Bei Chu <914745487@qq.com>
2024-04-08 00:07:43 +0800

    fix: Don't filter oracle V$LOG by rba (#2482)

--- dozer-ingestion/oracle/src/connector/replicate/log/redo/mod.rs
+++ dozer-ingestion/oracle/src/connector/replicate/log/redo/mod.rs
@@ -1,6 +1,6 @@
 use oracle::Connection;
 
-use crate::connector::Error;
+use crate::connector::{Error, Scn};
 
 /// Given a log file name, a redo reader emits `LogManagerContent` rows
 pub trait RedoReader {
@@ -8,13 +8,12 @@ pub trait RedoReader {
 
     /// Reads the `LogManagerContent` rows that have:
     ///
-    /// - scn >= start_scn
-    /// - rba > last_rba.0 || (rba == last_rba.0 && rbabyte > last_rba.1)
+    /// - scn > last_scn
     fn read<'a>(
         &self,
         connection: &'a Connection,
         log_file_name: &str,
-        last_rba: Option<(u32, u16)>,
+        last_scn: Option<Scn>,
         con_id: Option<u32>,
     ) -> Result<Self::Iterator<'a>, Error>;
 }
